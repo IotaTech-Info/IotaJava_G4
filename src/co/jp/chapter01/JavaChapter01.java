741800445f5efa2865d2debe50a3ce25a619cd81
十六夜ユキ <xylh1116@gmail.com>
2022-10-06 22:09:47 +0900

    homework01

--- src/co/jp/chapter01/JavaChapter01.java
+++ src/co/jp/chapter01/JavaChapter01.java
@@ -5,7 +5,16 @@ public class JavaChapter01 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-		System.out.println("hello world");
+		System.out.println("hello world!");
+		
+		
+		int i = 255;
+		System.out.println(i);	
 
+		int a = 10;
+		int b = 3;
+		 System.out.println("a/b:"+((float)a/b));	
+		
 	}
+
 }
