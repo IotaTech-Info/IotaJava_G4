f9e4d6c144668d33a5092c8b93832d0feccf2220
DAI <dylin0811@gmail.com>
2022-10-06 22:04:22 +0900

    質問集01解答

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -7,19 +7,18 @@ public class JavaHomeWork01 {
 	public static void main(String[] args) {
 
 		// 質問０１
-		// int i = 10;
-		// int n = i%5;
+		//int i = 10;
+	    //int n = i%5;
+		//n=0
 
-		// N = XXXXX
-
-		// 質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0;
-		// 回答XXXXXXX
+		// 質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0;		
+		// 変数aは10より大きい場合、aを11にする。そうでなければ、aを0にする。
 
 		// 質問３︓変数値を⽐較する演算⼦は = or == ?
-
+        //==
 
 		// 質問４︓ブリアン（ boolean ）変数に設定可能の値は「?」「?」。
-
+		//「true」または「false」
 
 	}
 
