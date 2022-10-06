1217869a62d08f134cb90bafe647c6e9676e6ade
a10 <a10@localhost>
2022-10-06 22:30:29 +0900

    20221006 wang

--- src/co/jp/chapter01/JavaChapter01.java
+++ src/co/jp/chapter01/JavaChapter01.java
@@ -1,11 +1,94 @@
 package co.jp.chapter01;
 
 public class JavaChapter01 {
+//	final static double pai = 3.1415926;
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-		System.out.println("hello world");
-
+//		System.out.println("hello world");
+//		
+//		int i = 100;
+//		
+//		
+//		
+//		
+//		
+//		int a = 10;
+//		float b = 3.0f;
+//		//+ - * /
+//		System.out.println("a+b" + (a + b));
+//		System.out.println("a-b" + (a - b));
+//		System.out.println("a*b" + (a * b));
+//		System.out.println("a/b" + (a / b));
+//		//低精度往高精度靠拢
+//		i++;++i;
+//      i--;--i; 
+		
+		
+		
+		
+		
+//		int x = 17;
+//		int y = 2; //0 1
+//		int yy = 3;//0 1 2
+//		
+//		System.out.println(x % y);
+		//取余数，判断奇数偶数 1 为奇数， 0 为偶数
+//		
+//		int x = 99;
+//		int y = 99;
+//		
+//		if(x > y) {
+//			System.out.println("x > y");
+//		}else {
+//			System.out.println("x <= y");
+//		}
+//		//比较有小于 大于 等于 三种情况
+//		
+//		if(x != y) {
+//			System.out.println("x == y");
+//		}
+		//双等号 是比较 单等号 是赋值
+		
+		
+		
+		
+		
+		
+		//&& and 
+		//true true true
+		//true false false
+		//false false false  两者又一个是false则为false
+		//|| or
+		//true true true   
+		//true false true
+		//false false false  两者有一个是true则为true
+		//！ 非
+		// true →　false
+		//false →　true
+	
+		
+		
+		
+//		
+//		int x = 100;
+//		int y = 50;
+//		int max = x > y ? x : y;
+//		//等于下面这个算式
+//		if(x < y) {
+//			max = x;
+//		}else {
+//			max = y;
+//		}
+//		
+//		System.out.println(max);
+		
+		//变数名得是字母开头，数字可以接在后面，有意义的符号不可以，下划线可以
+		
+		
+		
+				
+				
 	}
 }
