96770f4683f8f29a129a3b26b5d4523a55d5f99b
Alice <Alice@DESKTOP-C71DMB1>
2022-10-06 21:02:47 +0800

    01_xiong

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -9,16 +9,28 @@ public class JavaHomeWork01 {
 		// 質問０１
 		// int i = 10;
 		// int n = i%5;
-
 		// N = XXXXX
+		
+		int i = 10;
+		int n = i%5;
+				
+		System.out.println("質問01:"+ n);
+		// n = 0
+
 
 		// 質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0;
 		// 回答XXXXXXX
+		
+		System.out.println("質問02:a>10の場合には11を返す、ほかの場合は0を返す。");
 
 		// 質問３︓変数値を⽐較する演算⼦は = or == ?
+		
+		System.out.println("質問03:　==　");
 
 
 		// 質問４︓ブリアン（ boolean ）変数に設定可能の値は「?」「?」。
+		
+		System.out.println("質問04:「true」「false」");
 
 
 	}
