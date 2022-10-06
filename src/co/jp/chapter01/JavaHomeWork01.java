5212d8118ef554574ddffaaf80736b4142d19775
a10 <a10@localhost>
2022-10-06 23:03:05 +0900

    20221006 wangyiling

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -6,13 +6,13 @@ public class JavaHomeWork01 {
 
 	public static void main(String[] args) {
 
-		// 質問０１
+		// 質問１：
 		// int i = 10;
 		// int n = i%5;
 		// n = 0
 
 		// 質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0;
-		// a > 10を評価する、 true の場合、 11 を返す、 false の場合、0 を返し
+		// a > 10を評価する、 true の場合、 11 を返す、 false の場合、0 を返し。
 
 		// 質問３︓変数値を⽐較する演算⼦は = or == ?
 		//==です。
