130fd269e166fb74dfd6b2100c4a96f76a930d6a
dzw <dzw@duanzeweidembp2.airport>
2022-10-10 13:22:30 +0900

    段沢偉　10.6宿題

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -6,20 +6,21 @@ public class JavaHomeWork01 {
 
 	public static void main(String[] args) {
 
-		// 質問０１
+		// 質問１:以下変数nの値は？
 		// int i = 10;
 		// int n = i%5;
+		//解答：n= 0 
 
-		// N = XXXXX
 
 		// 質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0;
-		// 回答XXXXXXX
+		// 解答：もしa > 10の場合はaの値は11になります。それ以外の場合はaの値は０になります。
+
 
 		// 質問３︓変数値を⽐較する演算⼦は = or == ?
-
+        //解答：==
 
 		// 質問４︓ブリアン（ boolean ）変数に設定可能の値は「?」「?」。
-
+        //解答：[true],[false]
 
 	}
 
