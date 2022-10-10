1a4d60d531ae86883baf8f9ca67698ed4dfaa854
dzw <dzw@duanzeweidembp2.airport>
2022-10-10 13:16:35 +0900

    段沢偉　10.6宿題

--- src/co/jp/chapter01/JavaChapter01.java
+++ src/co/jp/chapter01/JavaChapter01.java
@@ -4,8 +4,14 @@ public class JavaChapter01 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		
+		int i = 10;
+		
+		int n =i%5;
+		
+		System.out.println(n);
 
-		System.out.println("hello world");
 
 	}
+
 }
