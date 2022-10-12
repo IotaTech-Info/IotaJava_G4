119d4f641b10494ade590a6385c7a38658e0a5a9
EB-li_biheng <114920630+Hitsune369@users.noreply.github.com>
2022-10-12 17:55:26 +0900

    Add files via upload

---
+++ JavaHomeWork01 EB-李必恒.java
@@ -0,0 +1,24 @@
+package co.jp.chapter01;
+
+public class JavaHomeWork01 {
+
+	static int num;
+
+	public static void main(String[] args) {
+
+		// 質問０１
+		// int i = 10;
+		// int n = i%5; 
+          
+		// N = XXXXX
+               答え ：　nは０
+		// 質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0;
+               
+           答え： もしaは10より大きい場合、１１はaに入れる、そうではない場合は０はaに入れる
+
+		// 質問３︓変数値を⽐較する演算⼦は = or == ?//
+
+                                 答え：＝＝
+		// 質問４︓ブリアン（ boolean ）変数に設定可能の値は「?」「?」。//
+
+                              答え：ture,false/0,1
