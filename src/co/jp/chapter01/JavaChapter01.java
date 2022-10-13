1d83ad80a0cb762b09fac09c18157f7be251a212
wuzhuangzhuang <wuzhuangzhuang@wuzhuanangdeair.elecom>
2022-10-13 18:54:22 +0900

    gososo

--- src/co/jp/chapter01/JavaChapter01.java
+++ src/co/jp/chapter01/JavaChapter01.java
@@ -4,6 +4,16 @@ public class JavaChapter01 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		
+//		質問１︓以下変数nの値は︖
+//		回答：n=0
+//		質問２︓右の式を解釈してください:a = a > 10  ? 11 : 0;
+//		回答：a > 10がtrueならa=11、falseならa=0
+//		質問３︓変数値を比較する演算子は=or==?
+//		回答：==
+//		質問４︓ブリアン（boolean）変数に設定可能の値は「?」「?」。
+//		回答：true,false
+
 
 		System.out.println("hello world");
 
