6d92b1bd335b6d29838ca00150306d5c0f28fa79
a10 <a10@localhost>
2022-10-13 22:23:31 +0900

    20221013wangyiling

--- src/co/jp/chapter02/JavaChapter02.java
+++ src/co/jp/chapter02/JavaChapter02.java
@@ -4,7 +4,166 @@ public class JavaChapter02 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+//		
+//		
+//		//int
+//		int i = 100;
+//		int j = 100;
+//		
+//		System.out.println(i == j);
+//		
+//		
+//		//文字列   以下两种方式
+//		String str1 = "abcdefg";
+//		System.out.println(str1);
+//		
+//		String str2 ="abcdefg";//
+//		System.out.println(str2);
+//		
+//		String str3 = new String("abcdefg");//2
+//		String str4 = new String("abcdefg");
+//		
+//		System.out.println(" == 比较" + (str1 == str2)); //true
+//		System.out.println(" == 比较" + (str3 == str2));//false
+//		System.out.println(" == 比较" + (str3 == str4));//false
+//		
+//		
+//		System.out.println(" equals 比较" + str1.equals(str2));
+//		System.out.println(" equals 比较" + str3.equals(str2));
+//		System.out.println(" equals 比较" + str3.equals(str4));
+//		
+//		//null 8大基本类型都不能设置null
+//		
+////		int a =a;
+//		
+//		String b = null;
+//		String c = null;
+//		
+//		JavaChapter02 obj = null;
+		
+//		System.out.println(b.equals(c));//null 无法比较
+//		
+//		//null　空object
+//		String a = null;
+//		//“”ブラック 空白 空文字列
+//		String b = "";
+//		//" " 半角
+//		String c = " ";
+//		//"　" 全角
+//		String d ="　";
+//		
+//		System.out.println(a);
+//		System.out.println("1" + b + "1" );//11
+//		System.out.println("1" + c + "1" );//1 1
+//		System.out.println("1" + d + "1" );//1　1
+//		
+//		System.out.println("abcdefg".length());//length字符数
+//		//全角半角都占一个char
+//		System.out.println("a b c d e f g".length());//13
+//		System.out.println("a　b　c　d　e　f　g".length());//13
+		//全角占2桁、半角占1桁
+		
+		
+//		
+//		int x = 1 + 1 ;
+//		
+//		System.out.println(x);
+//		
+//		String w = "1.0";
+//		
+//		String y = "a" + 1;
+//		String y2 =1 + "1";
+//		
+//		System.out.println(y);
+//		System.out.println(y2);
+		
+//		System.out.println("""");//""有意义（是字符串）所以无法直接导出
+		
+//		int x = 0;
+//		
+//		if(x >0) {
+//			x = x+ 1;
+//			System.out.println(x);
+//		}
+		
+//		else {
+//		
+//		}
+		//可以省略else
+		
+//		String sex = "男性";
+//		int age = 19;
+//		
+//		if("男性".equals(sex)) {
+//			if(age > 18) {
+//			System.out.println("man");
+//		}else {
+//			System.out.println("boy");
+//		}
+//		}else {
+//			if(age >18) {
+//				System.out.println("woman");
+//			}else {
+//				System.out.println("girl");
+//			}
+//		}
 
+//		int age = 6;
+//		
+//		if(age <=6 ){
+//			System.out.println("幼児:無料");
+//		}else if(age > 6 && age <= 12){
+//			System.out.println("小学生:100円");
+//		}else if(age > 12 && age <= 18){
+//			System.out.println("中高生:500円");
+//		}else{
+//			System.out.println("大人:1000円");
+//		}
+//		
+//		
+//		switch(age) {
+//		case 1 :
+//		case 2 :
+//		case 3 :
+//		case 4 :
+//		case 5 :
+//		case 6 :
+//			System.out.println("幼児:無料");
+//			break;
+//		case 7 :
+//		case 8 :
+//		case 9 :
+//		case 10:
+//		case 11 :
+//		case 12 :
+//			System.out.println("小学生:100円");
+//			break;
+//		case 13 :
+//		case 14 :
+//		case 15 :
+//		case 16:
+//		case 17 :
+//		case 18 :
+//			System.out.println("中高生:500円");
+//			break;
+//		case 19 :
+//		case 20 :
+//		case 21 :
+//		case 22:
+//		case 23 :
+//		case 24 :
+//			System.out.println("大人生:1000円");
+//			break;
+//			
+//		}
+//		
+//		
+		
+		
+		
+		
+		
+		
 	}
 
 }
--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -3,6 +3,35 @@ package co.jp.chapter02;
 public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
+		
+		//質問１︓ System.out.println(“this is line1\tthis line2“); は何⾏出⼒しますか︖
+		//1行
+		
+		//質問２︓Stringを⽐較⽅法は︖
+		//equals
+		
+		//質問３︓Float値⇒String⽂字列変更する場合、どうなメソッドを利⽤しますか
+		// 1.0F + ““
+		
+		//質問４︓以下式の値を記載してください。
+		//第一問 1.0 + 2.0 
+		//3.0	
+		//第二問 1.0 + “2.0“
+		//1.02.0
+		
+		//質問５︓以下ステートメントの出⼒を記載してください。
+//		public static void main(String[] args) {
+//			int a = 10;
+//			System.out.println("a");
+//		}
+		//ステートメントはaです。
+		
+			
+		
+		
+		
+		
+		
 
 	}
 }
