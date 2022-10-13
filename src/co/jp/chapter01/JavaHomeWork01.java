71b545b31fac9942751cff265e83b3487e393949
mazhiqiang26 <mazhiqiang529@yahoo.co.jp>
2022-10-13 16:33:19 +0900

    宿題

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -10,15 +10,15 @@ public class JavaHomeWork01 {
 		// int i = 10;
 		// int n = i%5;
 
-		// N = XXXXX
+		// N = 0
 
 		// 質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0;
-		// 回答XXXXXXX
+		// 回答XXXXXXX a>10を評価する。trueの場合11を返す。falseの場合、0を返す。
 
-		// 質問３︓変数値を⽐較する演算⼦は = or == ?
+		// 質問３︓変数値を⽐較する演算⼦は = or == ?　==
 
 
-		// 質問４︓ブリアン（ boolean ）変数に設定可能の値は「?」「?」。
+		// 質問４︓ブリアン（ boolean ）変数に設定可能の値は「true」「false」。
 
 
 	}
