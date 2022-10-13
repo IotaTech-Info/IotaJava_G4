1e7021764b2108d145e431f47dfedfa592c55682
Dolly <Dolly@DESKTOP-30A8GE6>
2022-10-13 22:18:19 +0900

    homework chapter2

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -3,6 +3,32 @@ package co.jp.chapter02;
 public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
-
+//	質問１
+//		1.1行
+		
+		
+		
+//	質問２
+//		2.equals
+		
+		
+		
+//	質問３
+//	2.1.0F + ""
+		
+		
+		
+//	質問４
+//	1.3.0
+//	2.1.02.0
+		
+		
+	
+//	質問５
+//	a
+		
+		
+		
+	
 	}
 }
