c69667dfbdc1be7afe6be3a7fa24b5643dbf3fce
LAW <LAW@DESKTOP-V60M247>
2022-10-13 21:31:35 +0800

    宿題2

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -3,6 +3,17 @@ package co.jp.chapter02;
 public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
-
+		
+		//質問1：答え　1行;
+		
+		//質問2：答え　equals();
+		
+		//質問3：答え　String.valueOf(1.0F);
+		
+		//質問4：1.0 + 2.01 = 3.0; 1.0 + "2.0" = 1.02.0;
+		
+		//質問5：答え　a;
+	
+		
 	}
 }
