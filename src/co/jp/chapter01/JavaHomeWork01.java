7ff92293f282901d11167bbf6e054a7d6dabd8df
EB-Zengwenjie <zengwenjie.reiam@gmail.com>
2022-10-13 12:18:16 +0900

    JavaHomeWork01_Zengwenjie

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -5,7 +5,7 @@ public class JavaHomeWork01 {
 	static int num;
 
 	public static void main(String[] args) {
-
+		
 		// 質問０１
 		// int i = 10;
 		// int n = i%5;
@@ -16,12 +16,11 @@ public class JavaHomeWork01 {
 		もし、aの値は10より小さい、aの値は0にします。 */
 
 		// 質問３︓変数値を⽐較する演算⼦は = or == ?
-		//回答：変数値を⽐較する演算⼦は ==　です。
+		//回答：変数値を⽐較する演算⼦は == です。
 
 
 		// 質問４︓ブリアン（ boolean ）変数に設定可能の値は「?」「?」。
 		//回答：ブリアン（ boolean ）変数に設定可能の値は「ture」「false」
-
 
 	}
 
