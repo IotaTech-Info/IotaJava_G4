903d7cbbb1a7072f162654b70e4599f10dcfa1a2
cuiguangpuxiu <cgpx891@gmail.com>
2022-10-13 12:44:03 +0900

    宿題を提出します。

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -10,15 +10,22 @@ public class JavaHomeWork01 {
 		// int i = 10;
 		// int n = i%5;
 
-		// N = XXXXX
+		// n = XXXXX   n=0
 
+		int i = 10;
+		System.out.println("n =" + (i % 5));
+		
+		
 		// 質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0;
-		// 回答XXXXXXX
+		// 回答XXXXXXX　aは10より大きければ,aに11を代入します。aは10以下であれば、aに0を代入します。
 
+		
 		// 質問３︓変数値を⽐較する演算⼦は = or == ?
+		//  「==」です。
 
 
 		// 質問４︓ブリアン（ boolean ）変数に設定可能の値は「?」「?」。
+		//「0」「1」です。
 
 
 	}
