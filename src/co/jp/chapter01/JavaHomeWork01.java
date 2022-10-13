32538ca597bda5e6be80fed6a90bede82381d8e6
ZHANG Boyang <zhanginjp@srmtlab.org>
2022-10-13 16:59:01 +0900

    Revert "Update JavaHomeWork01.java"
    This reverts commit 8992d34320c2cc3eca8262534a407e77437543c4.

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -6,23 +6,20 @@ public class JavaHomeWork01 {
 
 	public static void main(String[] args) {
 
-		// 質問１
+		// 質問０１
 		// int i = 10;
 		// int n = i%5;
 
-		// 回答：nの値は0
+		// N = XXXXX
 
 		// 質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0;
-		
-		// 回答：aの値が10より大きい場合は11にする、そうではなければ0とする。
+		// 回答XXXXXXX
 
-		// 質問３︓変数値を比較する演算子は = or == ?
+		// 質問３︓変数値を⽐較する演算⼦は = or == ?
 
-		// 回答：==
 
 		// 質問４︓ブリアン（ boolean ）変数に設定可能の値は「?」「?」。
 
-		// 回答：True False
 
 	}
 
