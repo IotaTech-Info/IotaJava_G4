e74f7681be8b1dafcd5b91a89b3ed987c13b1d2b
xuzek <xuzek@192.168.2.100>
2022-10-13 21:43:25 +0900

    HomeWork02

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -3,6 +3,26 @@ package co.jp.chapter02;
 public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
-
+//		質問1
+		
+		1. 1行
+		
+//		質問2
+		
+		2. equals
+		
+//		質問3
+		
+		2. 1.0F+""
+		
+//		質問4
+		
+		1. 3.0
+		2. 1.02.0
+		
+//		質問5
+		
+		a
+		
 	}
 }
