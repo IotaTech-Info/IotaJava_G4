da88b15115d059595efb447bd8c1799308839d2c
zyr96 <zyr96@192.168.10.135>
2022-10-14 15:51:18 +0900

    homework java02

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -4,5 +4,22 @@ public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
 
+	//質問１，	
+    System.out.println("this is line1\tthis line2");
+    
+    // 回答　 1.1行
+    
+   //質問２　Stringを⽐較⽅法は︖
+   //回答　2．equals
+    
+    //質問３．Float値⇒String⽂字列変更する場合、どうなメソッドを利⽤しますか
+    //回答 2.String.valueOf(1.0F)
+    
+    //質問４︓以下式の値を記載してください。
+    //回答　3.0　　　1.02.0
+    
+    
+    //質問５︓以下ステートメントの出⼒を記載してください。
+    //回答　a
 	}
 }
