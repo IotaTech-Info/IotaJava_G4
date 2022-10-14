cec766283f098ae90adbb43a6ecd201f883d9869
liuxiaoyang98 <liuxiaoyang98@gmail.com>
2022-10-14 15:48:21 +0900

    宿題の提出

--- src/co/jp/chapter01/JavaChapter01.java
+++ src/co/jp/chapter01/JavaChapter01.java
@@ -4,5 +4,6 @@ public class JavaChapter01 {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		
 	}
 }
--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -3,6 +3,53 @@ package co.jp.chapter02;
 public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
+		System.out.println("this is line1\tthis line2");
+		System.out.println(1.0F+" ");
+		System.out.println(String.valueOf("1.0F"));
+		System.out.println( 1.0 + 2.0);
+		System.out.println( 1.0 + "2.0");
+		/*
+		 *  
+		 *  質問１︓ System.out.println(“this is line1\tthis line2“); は何⾏出⼒しますか︖
+			1. １⾏
+			2. ２⾏
+			
+			答：「１行」です
+			
+			
+			質問２︓Stringを⽐較⽅法は︖
+			1. ==
+			2. equals
+			
+			答：　２番「equals」です
+			
+			
+			質問３︓Float値⇒String⽂字列変更する場合、どうなメソッドを利⽤しますか
+			1. String.valueOf(“1.0F“)
+			2. 1.0F + ““
+			
+			答：　１番です
+			
+			
+			質問４︓以下式の値を記載してください。
+			1. 1.0 + 2.0
+			2. 1.0 + “2.0“
+			
+			1：3.0
+			2：1.02.0
+			
+			
+			質問5:以下ステートメントの出力を記載してください。
+			
+			public static void main(String[] args) {
+				int a=10;
+				System.out.println("a");
+				
+			答：　出力は 「a」　です
+		 * 
+		 * 
+		 */
+		
 
 	}
 }
