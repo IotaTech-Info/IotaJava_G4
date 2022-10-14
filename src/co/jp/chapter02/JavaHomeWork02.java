fc8e8dd21f37900ab2a9434d22951e0aff473360
Nakishigure <Nakishigure@DESKTOP-PDCECSS>
2022-10-14 17:39:48 +0900

    宿題２

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -3,6 +3,17 @@ package co.jp.chapter02;
 public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
-
+		/**
+		 * 回答１：１行
+		 *　
+		 *　回答２：.equals();
+		 *
+		 *　回答３：String.valueOf(“1.0F“)
+		 *
+		 *　回答４：1、3.0
+		 *		 2、1.02.0
+		 *
+		 *　回答５：a
+		 */
 	}
 }
