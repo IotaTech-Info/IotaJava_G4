4e06d312c0e5248319f7974acad80c676e4dfaa0
DAI <dylin0811@gmail.com>
2022-10-15 18:18:38 +0900

    質問集02の解答

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -3,6 +3,19 @@ package co.jp.chapter02;
 public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
-
+		
+//		質問１︓ System.out.println(“this is line1\tthis line2“); は何行出力しますか︖
+//		１行
+//		
+//		質問２︓Stringを比較方法は︖
+//		equals
+		
+//		質問３︓Float値⇒String文字列変更する場合、どうなメソッドを利用しますか
+//		1.0F + ““
+//		質問４︓以下式の値を記載してください。
+//		1. 	3.0
+//		2.	1.02.0
+//		質問５︓以下ステートメントの出力を記載してください。
+//		a
 	}
 }
