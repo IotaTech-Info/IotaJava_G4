4af3d20eaed771d137ebb5b7440e588bb8da63e0
prfme <prfme@LAPTOP-7L9MIPB5>
2022-10-17 16:17:49 +0900

    homework2

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -4,5 +4,22 @@ public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
 
+		//質問1
+		//答え:1
+
+		
+		//質問2
+		//答え：2
+		
+		//質問3
+		//答え:2
+		
+		//質問4 1
+		//答え:1番→3.0　2番→"1.02.0"
+		
+		//質問5
+		//答え:a
+		
+	
 	}
 }
