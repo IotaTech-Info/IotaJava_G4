6579de88abb2e172ab9dde6228b2d6e90f60dd44
szy19 <szy19@Fate>
2022-10-17 20:13:08 +0900

    HomeWork03

--- src/co/jp/chapter03/JavaHomeWork03.java
+++ src/co/jp/chapter03/JavaHomeWork03.java
@@ -4,7 +4,56 @@ public class JavaHomeWork03 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
-
+		JavaHomeWork03 test=new JavaHomeWork03();
+		test.Q1();
+		test.Q2();
 	}
+	public void Q1() {
+		int a=56;
+		System.out.println("成績ランキング判断。入力値="+a);
+		if (a>90) {
+			System.out.println("A");
+		}
+		else if (a>=80&&a<90) {
+			System.out.println("B");
+		}
+		else if (a>=80&&a<90) {
+			System.out.println("C");
+		}
+		else if (a>=80&&a<90) {
+			System.out.println("D");
+		}
+		else {
+			System.out.println("E");
+		}
+	}
+	public void Q2() {
+		int m=3340;
+		int c1=0;
+		int c5=0;
+		int c10=0;
+		int c100=0;
+		int c500=0;
+		if (m>500) {
+			c500=m/500;
+			m=m%500;
+		}
+		if (m>100) {
+			c100=m/100;
+			m=m%100;
+		}
+		if (m>10) {
+			c10=m/10;
+			m=m%10;
+		}
+		if (m>5) {
+			c5=m/5;
+			m=m%5;
+		}
+		c1=m;
+		System.out.println("1円は"+c1+"　5円は"+c5+"　10円は"+c10+"　"
+				+ "　100円は"+c100+"　500円は"+c500+"枚あります");
+		
+	}
+	
 }
