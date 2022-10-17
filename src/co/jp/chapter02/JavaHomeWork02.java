bde6866322ce32f400df0f099045dd8aa69a75ca
DDG-Edgerunners <jyzhou970402@outlook.com>
2022-10-17 16:12:30 +0900

    homework2

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -4,5 +4,20 @@ public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
 
+		//質問１
+		//答え: 1
+		
+		//質問２
+		//答え: 2
+		
+		//質問３
+		//答え: 1と2
+		
+		//質問４
+		//答え: 1番 ⇒ 3.0  2番　⇒　"1.02.0"	
+		
+		//質問５
+		//答え；a
+		
 	}
 }
