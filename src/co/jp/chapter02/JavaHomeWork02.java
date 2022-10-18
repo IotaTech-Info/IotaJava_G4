8d5e93f8d66db55b21feaab47d51b94f87fa79cc
dzw <dzw@duanzeweidembp2.airport>
2022-10-19 01:08:16 +0900

    HomeWork02

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -3,18 +3,28 @@ package co.jp.chapter02;
 public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
-		int l = 11;
-		
-		for(int i = 0;i < l;i++) {
-			for(int j = 0;j<l;j++) {
-				if(i==j||i+j == 10 ||i==5||j==5) {
-					System.out.print(""+i+j+"  ");
-				}else {
-					System.out.print("   ");
-				}
-			}
-			System.out.println();
-		}
+//		質問＆練習 
+//		質問１︓ System.out.println(“this is line1\tthis line2“); は何⾏出⼒しますか︖ 1. １⾏ 2. ２⾏ 
+//		解答：1行
+//
+//		質問２︓Stringを⽐較⽅法は︖ 1. == 2. equals 
+//		解答：equals（）
+//
+//		質問３：Float値⇒String⽂字列変更する場合、どうなメソッドを利⽤しますか 1. String.valueOf(“1.0F“) 2. 1.0F + ““ 
+//		解答：1. String.valueOf(“1.0F“)
+//
+//		質問４︓以下式の値を記載してください。
+//		1. 1.0 + 2.0 
+//		解答：3.0
+//		2. 1.0 + “2.0“ 
+//		解答：”1.02.0”
+//
+//		質問５︓以下ステートメントの出⼒を記載してください。 
+//		 public static void main(String[] args) { 
+//		   int a = 10; System.out.println("a"); 
+//		}
+//
+//		解答：a
 
 	}
 }
