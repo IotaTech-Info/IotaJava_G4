8a7fdd37bacdbf8762532d3ee452d926abe1a272
daisy <luyuwen.tokyo@gmail.com>
2022-10-18 19:14:26 +0900

    20221018 luyuwen

--- src/co/jp/chapter03/JavaHomeWork03.java
+++ src/co/jp/chapter03/JavaHomeWork03.java
@@ -7,13 +7,13 @@ public class JavaHomeWork03 {
 //		質問1:成績ランキング判断を作成する「90以上:A, 80-90:B, 70-80:C, 60-70: D, 60以下:E」
 //		int a = 56;
 //		System.out.println("成績ランキング判断。入力値=" + a);
-//		if (a > 90) {
+//		if (a >= 90) {
 //			System.out.println("A");
-//		} else if (a > 80) {
+//		} else if (a >= 80) {
 //			System.out.println("B");
-//		} else if (a > 70) {
+//		} else if (a >= 70) {
 //			System.out.println("C");
-//		} else if (a > 60) {
+//		} else if (a >= 60) {
 //			System.out.println("D");
 //		} else {
 //			System.out.println("E");
