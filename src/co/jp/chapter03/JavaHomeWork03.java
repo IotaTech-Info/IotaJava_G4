bb798984ad18adc929140b7d471210e92d069509
daisy <luyuwen.tokyo@gmail.com>
2022-10-18 19:05:33 +0900

    20221018 luyuwen

--- src/co/jp/chapter03/JavaHomeWork03.java
+++ src/co/jp/chapter03/JavaHomeWork03.java
@@ -4,7 +4,52 @@ public class JavaHomeWork03 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
-
+//		質問1:成績ランキング判断を作成する「90以上:A, 80-90:B, 70-80:C, 60-70: D, 60以下:E」
+//		int a = 56;
+//		System.out.println("成績ランキング判断。入力値=" + a);
+//		if (a > 90) {
+//			System.out.println("A");
+//		} else if (a > 80) {
+//			System.out.println("B");
+//		} else if (a > 70) {
+//			System.out.println("C");
+//		} else if (a > 60) {
+//			System.out.println("D");
+//		} else {
+//			System.out.println("E");
+//		}
+		
+		
+//		質問2:日本のコインを1円、5円、10円、100円、500円があります。3340 円のコイン数をもとめください。
+//		int c = calculate(3340);
+//		System.out.println(c);
+//	}
+//
+//	public static int calculate(int total) {
+//	    int c = 0;
+//	    while(total != 0) {
+//	       if(total >= 500) {
+//	           c = c + total / 500;
+//	           total = total % 500;
+//	       }
+//	       if (total >= 100) {
+//	           c = c + total / 100;
+//	           total = total % 100;
+//	       }
+//	       if (total >= 10) {
+//	           c = c + total / 10;
+//	           total = total % 10;
+//	       }
+//	       if (total >= 5) {
+//	           c = c + total / 5;
+//	           total = total % 5;
+//	       }
+//	       if (total >= 1) {
+//	           c = c + total / 1;
+//	           total = total % 1;
+//	       }
+//	    }
+//
+//	    return c;
 	}
 }
