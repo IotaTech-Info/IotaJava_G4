1bd916405a32359a9cd8a99ed3e3853ec4c279c9
DDG-Edgerunners <jyzhou970402@outlook.com>
2022-10-18 20:21:49 +0900

    Homework03

--- src/co/jp/chapter03/JavaChapter03.java
+++ src/co/jp/chapter03/JavaChapter03.java
@@ -4,6 +4,19 @@ public class JavaChapter03 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		for(int k = 0; k < 11; k++) {
+			   for(int s = 0; s < 11; s++) {
+			    if(k == s || k + s == 10|| k==5 || s==5 ) {
+			    	if(s==10||k==10) {
+			    		System.out.print("" + k + s + "");
+			    	}else {
+			    		System.out.print("" + k + s + " ");
+			    	}
+			    }else {
+			     System.out.print("   ");
+			    }
+			   }
+			   System.out.println();
+			  }
 	}
 }
--- src/co/jp/chapter03/JavaHomeWork03.java
+++ src/co/jp/chapter03/JavaHomeWork03.java
@@ -4,7 +4,65 @@ public class JavaHomeWork03 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
-
+//		int a = 56;
+//		System.out.println("成績ランキング判断。入力値＝"+ a );
+//		if(a>=90) {
+//			System.out.println("A");
+//		}else if(a>=80&&a<90) {
+//			System.out.println("B");
+//		}else if(a>=70&&a<80) {
+//			System.out.println("C");
+//		}else if(a>=60&&a<70) {
+//			System.out.println("D");
+//		}else {
+//			System.out.println("E");
+//		}
+//
+//		コンソール:
+//		成績ランキング判断。入力値＝56
+//		E
+//		
+		int coins= 3377;
+		coin_compute(coins);
+		
 	}
+	public static void coin_compute(int coins) {
+		int nokori_coins = 0;
+		int num_500=0,num_100=0,num_50=0,num_10=0,num_5=0,num_1 = 0;
+		for (;;) {
+			num_500 = coins/500;
+			nokori_coins = coins % 500;
+			if (nokori_coins == 0) {
+				break;
+			}
+			num_100 = nokori_coins/100;
+			nokori_coins = coins % 100;
+			if (nokori_coins == 0) {
+				break;
+			}
+			num_50 = nokori_coins/50;
+			nokori_coins = coins % 50;
+			if (nokori_coins == 0) {
+				break;
+			}
+			num_10 = nokori_coins/10;
+			nokori_coins = coins % 10;
+			if (nokori_coins == 0) {
+				break;
+			}
+			num_5 = nokori_coins/5;
+			nokori_coins = coins % 5;
+			if (nokori_coins == 0) {
+				break;
+			}
+			num_1 = nokori_coins/1;
+			nokori_coins = coins % 1;
+			if (nokori_coins == 0) {
+				break;
+			}
+		}
+		
+		System.out.println("500円の枚数は"+ num_500 +", "+ "100円の枚数は"+ num_100+", " + "50円の枚数は"+ num_50+", "+"10円の枚数は"+ num_10+", "+"5円の枚数は"+ num_5+", "+"1円の枚数は"+ num_1+".");
+		}
 }
+	
