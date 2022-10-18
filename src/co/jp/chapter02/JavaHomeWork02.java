fa8c616440625cd3a377b967e9240eba4e8013ac
dzw <dzw@duanzeweidembp2.airport>
2022-10-19 01:07:12 +0900

    HomeWork02

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -3,6 +3,18 @@ package co.jp.chapter02;
 public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
+		int l = 11;
+		
+		for(int i = 0;i < l;i++) {
+			for(int j = 0;j<l;j++) {
+				if(i==j||i+j == 10 ||i==5||j==5) {
+					System.out.print(""+i+j+"  ");
+				}else {
+					System.out.print("   ");
+				}
+			}
+			System.out.println();
+		}
 
 	}
 }
