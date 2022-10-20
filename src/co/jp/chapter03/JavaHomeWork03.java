9dfa77b9e58efeeb017f46918f345ad6b75abc2c
a10 <a10@localhost>
2022-10-20 21:37:28 +0900

    20221013wangyiling

--- src/co/jp/chapter03/JavaHomeWork03.java
+++ src/co/jp/chapter03/JavaHomeWork03.java
@@ -5,6 +5,95 @@ public class JavaHomeWork03 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		// 米字
+//		for (int i = 0; i < 11; i++) {
+//			for (int j = 0; j < 11; j++) {
+//				if (i == j || i + j == 10 || i == 5 || j == 5) {
+//					System.out.print("" + i + j + "  ");
+//				} else {
+//					System.out.print("    ");
+//				}
+//
+//			}
+//			System.out.println();
+//		}
+
+		// 菱形
+//        for( int i = 0; i <= 11; i++){
+//           for( int j = 0; j <= 11 ; j++){
+//				if(j== 5 - i || i == j -6 || j == i - 6 || i + j == 17 ) {
+//					System.out.print(" "+ i + j + "  "); 
+//				}else {
+//					System.out.print("    ");	
+//				} 
+//				
+//            }
+//			System.out.println();
+//		}
+
+		// 双菱形
+//		for (int i = 0; i <=11; i++) {
+//			for (int j = 0; j <=11; j++) {
+//				if (j == 5 - i || i- j == 6 || j - i ==  6 || i + j == 17) {
+//					System.out.print("" + i + j + " ");
+//				} else {
+//					if (i >= 3 && i <= 8 && j >= 3 && j <= 8) {
+//						if (i +j == 8 || j - i == 3 || i - j == 3 || i + j == 14) {
+//							System.out.print("" + i + j + " ");
+//						} else {
+//							System.out.print("   ");
+//						}
+//					} else {
+//						System.out.print("   ");
+//					}
+//				}
+//			}
+//			System.out.println();
+//		}
+
+		// 質問１︓成績ランキング判断を作成する「90以上︓A, 80-90︓B, 70-80︓C, 60-70︓ D, 60以下︓E」
+//		int a = 56;
+//		System.out.println("成績ランキング判断。入力値=" + a);
+//		// 以下コードを完成してください。
+
+//		int a = 56;
+//
+//		if (a >= 90) {
+//			System.out.println("A");
+//		} else if (a >= 80 && a < 90) {
+//			System.out.println("B");
+//		} else if (a >= 70 && a < 80) {
+//			System.out.println("C");
+//		} else if (a >= 60 && a < 70) {
+//			System.out.println("D");
+//		} else {
+//			System.out.println("E");
+//		}
+
+		// 質問２︓⽇本のコインを１円、５円、１０円、１００円、５００円があります。３３４7 円のコイン数をもとめください。
+
+		int money = 3347;
+		int coin500 = 0; 
+		int coin100 = 0;
+		int coin10 = 0;
+		int coin5 = 0;
+		int coin1 = 0;
+		
+		
+		coin500 = money / 500;
+		coin100 = money % 500 / 100;
+		coin10 = money % 500 % 100  / 10;
+		coin5 = money % 500 % 100 %10 / 5;
+		coin1 = money % 500 % 100 % 10 % 5 / 1;
+		
+		System.out.print("500円は" + coin500 +"枚,");
+		System.out.print("100円は" + coin100 +"枚,");
+		System.out.print("10円は" + coin10 +"枚,");
+		System.out.print("5円は" + coin5 +"枚,");
+		System.out.print("1円は" + coin1 +"枚です");
+					
+
+
 
 	}
 }
