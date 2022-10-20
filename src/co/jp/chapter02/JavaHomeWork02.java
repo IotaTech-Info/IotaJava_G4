8372e43d6daf4a917f247c147ade6f12e9fecaad
EB-li_biheng <114920630+Hitsune369@users.noreply.github.com>
2022-10-20 19:51:20 +0900

    Add files via upload

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -3,6 +3,52 @@ package co.jp.chapter02;
 public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
-
+		
 	}
 }
+
+
+///
+//質問１︓ System.out.println(“this is line1\tthis line2“); は何⾏出⼒しますか︖
+///1. １行
+///2. ２行
+                       答： 1行
+
+///質問２︓Stringを⽐較⽅法は︖
+///1. ==
+//2. equals
+                       答： equals
+/////
+
+//質問３︓Float値⇒String⽂字列変更する場合、どうなメソッドを利⽤しますか
+//1. String.valueOf(“1.0F“)
+//2. 1.0F + ““
+
+                       答え：1.0F + ““
+///
+///質問４︓以下式の値を記載してください。
+///1. 1.0 + 2.0
+///2. 1.0 + “2.0“
+     　　　　    答：1、 3.0       2、 1.02.0
+
+/////
+
+
+////質問５︓以下ステートメントの出⼒を記載してください。
+///いいね 1 番に「いいね」しましょう
+///⽂字 説明
+//\' '
+///\" “
+///\\ \
+///\t Tab
+///\n 改⾏
+///1
+///2
+///3
+////4
+////public static void main(String[] args) {
+//// int a = 10;
+ ///System.out.println("a");
+////}////
+           
+　　　　　　　　　　　　      答：a
