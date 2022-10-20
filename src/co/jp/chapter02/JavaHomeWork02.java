1a02673aea1d845aa6c8f77188c37890fc1cbcbc
cuiguangpuxiu <cgpx891@gmail.com>
2022-10-20 22:12:39 +0900

    宿題2

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -4,5 +4,39 @@ public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
 
+		//質問1:System.out.println(“this is line1 \t this line2");何行出力しますか
+		//回答:1行
+		
+		//質問２︓Stringを比較方法は︖
+		//回答:equals
+		
+		//質問３︓Float値⇒String文字列変更する場合、どうなメソッドを利用しますか
+		//回答： String.valueOf(“1.0F“)
+		
+		//質問４︓以下式の値を記載してください。
+		//1. 1.0 + 2.0
+		//2. 1.0 + "2.0" 
+		
+		//回答: 3.0 と 1.02.0
+		
+		double x = 1.0 + 2.0;
+		System.out.println(x);
+		
+		
+	    String y = 1.0 + "2.0";
+		System.out.println(y);
+		
+		
+		
+		//質問５︓以下ステートメントの出力を記載してください。
+		//1 public static void main(String[] args) {
+		//2 int a = 10;
+		//3 System.out.println("a");
+		//4 }
+		
+		//回答:a
+	
+		
+		
 	}
 }
