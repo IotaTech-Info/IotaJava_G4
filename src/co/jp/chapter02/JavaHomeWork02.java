2e04d7b7ebf91508263dd9d4b186d30779258b7d
donghe <donghe@localhost>
2022-10-21 16:48:34 +0900

    # WARNING: head commit changed in the meantime
    董赫

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -1,8 +1,35 @@
 package co.jp.chapter02;
 
-public class JavaHomeWork02 {
+/*
+質問＆練習
+質問１︓ System.out.println(“this is line1\tthis line2“); は何⾏出⼒しますか︖
+答え：１
+1. １⾏
+2. ２⾏
+質問２︓Stringを⽐較⽅法は︖
+答え：２
+1. ==
+2. equals
 
-	public static void main(String[] args) {
+質問３︓Float値⇒String⽂字列変更する場合、どうなメソッドを利⽤しますか
+答え：２
+1. String.valueOf(“1.0F“)
+2. 1.0F + ““
 
-	}
-}
+質問４︓以下式の値を記載してください。
+　float a = 1.0
+  float b = 2.0
+  string c ="2.0"
+  system.out.println{a+b}
+  system.out.println{a+c}
+1. 1.0 + 2.0
+2. 1.0 + “2.0“
+
+質問５︓以下ステートメントの出⼒を記載してください。
+
+答え：a
+
+public static void main(String[] args) {
+ int a = 10;
+ System.out.println("a");
+	*/
