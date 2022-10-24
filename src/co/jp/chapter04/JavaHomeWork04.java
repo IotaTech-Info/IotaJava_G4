738ad56ccbc228ac61621ebb45e8d7be39f929d3
daisy <luyuwen.tokyo@gmail.com>
2022-10-24 19:34:29 +0900

    20221024luyuwen

--- src/co/jp/chapter04/JavaHomeWork04.java
+++ src/co/jp/chapter04/JavaHomeWork04.java
@@ -4,6 +4,76 @@ public class JavaHomeWork04 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		//		問題1:int[] array = new int[4]; 各要素の値を記載してください。
+		//		0,0,0,0
+
+		//		問題2:int[] array = new int[] {5, 6}; 各要素の値を記載してください。
+		//		5,6
+
+		//		問題3:int[][] array = new int[2][3]; 各要素の値を記載してください。
+		//		{{0,0,0},{0,0,0}}
+
+		//		問題4:以下2重Foreachを理解してください。
+		//		int[][] array2d = new int[4][2];
+		//		for(int[]out:array2d) {
+		//			for(int inner:out){
+		//				System.out.println(inner);	
+		//			}
+		//		}
+		//		for(int[]out:array2d):２つ要素の配列を出力する。
+		//		for(int inner:out):配列の要素を出力する。
+		//		System.out.println(inner):array2d配列すべての要素を出力する
+
+		//      問題5:int[] array = new int[] {1, 3, 5, 7, 100, 0, 1}; 左の配列の最大値と最小桁値を System.out.println()。
+		//int[] array = new int[] { 1, 3, 5, 7, 100, 0, 1 };
+		//int min = array[0];
+		//int max = array[0];
+		//for (int i = 1; i < array.length; i++) {
+		//	if (array[i] < min) {
+		//		min = array[i];
+		//	}
+		//	if (array[i] > max) {
+		//		max = array[i];
+		//	}
+		//}
+		//System.out.println("最小桁値=" + min);
+		//System.out.println("最大値=" + max);
+
+		//      問題6:問題1の配列のSUMを求める。
+		//int[] array = new int[4];
+		//int sum = 0;
+		//for (int i = 0; i < array.length; i++) {
+		//	sum = sum + array[i];
+		//}
+		//System.out.println(sum);
+
+		//      問題7:行列(2次元配列)の和と積を計算してください。
+//		int[][] array1 = new int[][] { { 1, 2 }, { 3, 4 } };
+//		int[][] array2 = new int[][] { { 5, 6 }, { 7, 8 } };
+//		int[][] sum = new int[][] { { 0, 0 }, { 0, 0 } };
+//		int[][] mul = new int[][] { { 0, 0 }, { 0, 0 } };
+//		for (int i = 0; i < array1.length; i++) {
+//			for (int j = 0; j < array1[i].length; j++) {
+//				sum[i][j] = array1[i][j] + array2[i][j];
+//				mul[i][j] = array1[i][j] * array2[i][j];
+//			}
+//		}
+//
+//		System.out.println("和は:");
+//		for (int i = 0; i < array1.length; i++) {
+//			for (int j = 0; j < array1[i].length; j++) {
+//				System.out.print(sum[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
+//
+//		System.out.println("積は:");
+//		for (int i = 0; i < array1.length; i++) {
+//			for (int j = 0; j < array1[i].length; j++) {
+//				System.out.print(mul[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
 
 	}
 }
