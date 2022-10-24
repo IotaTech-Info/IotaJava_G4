68dbed2b6ae61f5edb0fd46c4277c4c496e63974
zyr96 <zyr96@LAPTOP-PNH4F107>
2022-10-25 00:03:02 +0900

    homework04

--- src/co/jp/chapter04/JavaHomeWork04.java
+++ src/co/jp/chapter04/JavaHomeWork04.java
@@ -5,5 +5,65 @@ public class JavaHomeWork04 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		
+		//問１．int[] array = new int[4];各要素の値
+		//回答．｛0,0,0,0}
+		
+		
+		//問２.  int[]array = new int[] {5,6};各要素の値
+		//回答.  5.6
+		
+		
+		//問3.  int [][]array = new int [2][3];各要素の値
+		//回答  0,0
+		//      0,0
+		//      0,0
+		
+		
+		//問４．
+		//int[][] array2d = new int[4][2];
+		//for (int[]out: array2d) {
+			//for(int inner :out) {
+				//System.out.println(inner);			}
+	    //回答　「４，２」の二次元配列を初期化した。
+		//　　　　このarray2dの要素をすべて出力したら、値が与えていないから０を8個出力した。
+		
+		
+		//問題5
+		// int[] array = new int[]{1,3,5,7,100,0,1};
+		//左の配列の最大値と最小桁値を出力
+		
+		//回答　int[] array = new int[]{1,3,5,7,100,0,1};
+		//int index = 0;
+       // for (int i=0;i<= arr.length-1 ; i++){
+         //   index = (i+1 < arr.length && arr[index] <= arr[i+1]) ? i+1 : index ;
+        //}
+        //System.out.println("最大值为："+arr[index]);
+	
+	//問題６
+	//	問題１のSUM
+		
+	//回答　０
+		
+	//問題７
+	//行列の和と積を計算	
+	//回答 
+	//	int[][] arr =new int [5][5];
+		//System.out.println(Arrays.toString(arr));
+		//for (int i=0; i<arr.length; i++) {
+		//	for(int j=0;j< arr[i].length;j++) {
+		//		System.out.print(arr[i][j]);
+		//	}
+		// system.out.println()
+		
+		
+		
+	}
+	 
+		
+		
+		
+		
+		
 	}
 }
