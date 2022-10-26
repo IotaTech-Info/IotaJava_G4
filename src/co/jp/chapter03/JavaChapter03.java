27bcde17510438ff315cd4759dbc3197bce501b7
11564 <11564@192.168.0.17>
2022-10-26 21:04:50 +0900

    homework6

--- src/co/jp/chapter03/JavaChapter03.java
+++ src/co/jp/chapter03/JavaChapter03.java
@@ -3,7 +3,23 @@ package co.jp.chapter03;
 public class JavaChapter03 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		int l = 12;
+		int mid = l/2 ;
+		for(int i = 0; i < l; i++) {
+			for(int j = 0; j < l; j++) {
+				if( i  <= mid &&  j <= mid && i + j == mid - 1 )
+					System.out.print("" + i + j + "\t");
+				else if ( i <= mid && j >= mid &&  j - i == mid )
+					System.out.print("" + i + j + "\t");
+				else if( i >= mid && j <= mid && i - j == mid )
+					System.out.print("" + i + j + "\t");
+				else if(i >= mid && j >= mid && i + j == l + mid - 1 )
+					System.out.print("" + i + j + "\t");
+				else
+					System.out.print("        ");
+			}
+			System.out.println();
+		}
 
 	}
 }
