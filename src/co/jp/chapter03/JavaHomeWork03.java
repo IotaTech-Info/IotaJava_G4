f084ed36cd973bfc668d54bd12f9a1d947b30732
wuzhuangzhuang <wuzhuangzhuang@wuzhuanangdeair.elecom>
2022-10-28 21:26:42 +0900

    １１２２

--- src/co/jp/chapter03/JavaHomeWork03.java
+++ src/co/jp/chapter03/JavaHomeWork03.java
@@ -22,4 +22,28 @@ public class JavaHomeWork03 {
 			System.out.println("E");
 		}
 	}
+	
+//	質問２
+//	public static void main(String[] args) {
+//		// TODO 自動生成されたメソッド・スタブ
+//    int b =3340;
+//    System.out.println("金額は"+b+"円です");
+//		int t;
+//		int kinsyu[] = {500,100,10,5,1};
+//		for (int i = 0; i < 4; i++) {
+//			t= b/kinsyu[i];
+//			if (t>0) {
+//				System.out.println(kinsyu[i]+"円は"+t+"枚です");
+//				b=b-t*kinsyu[i];
+//				
+//			}
+//		}
+//		
+//	}
+
+	
+	
+	
+	
+	
 }
