bb101aadcfe06e814ec9e302a7d6c59d1e00a5e1
donghe <donghe@localhost>
2022-10-28 17:55:52 +0900

    homework04

--- src/co/jp/chapter04/JavaHomeWork04.java
+++ src/co/jp/chapter04/JavaHomeWork04.java
@@ -5,5 +5,32 @@ public class JavaHomeWork04 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		/*
+		 1,  0,0,0,0
+		 2,  5,6
+		 3,000
+		   000
+		 4,通过扩张for循环取出每一个位置的数字
+		 5，
+		 6，int[] arry= new int[4];
+		    for (int x: array){
+		      add(x)};
+		 7. for(int[] out : array){
+		    for (int inner : out){
+		         add(innner);
+		         mul(innner):
+		         }
+		 
+		 
+		 
+		 
+		 
+		 */
+		
+		
+		
+		
+		
+		
 	}
 }
