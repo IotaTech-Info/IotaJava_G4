a3803cfcaf280600c99ee37f45a50212003a9702
daisy <luyuwen.tokyo@gmail.com>
2022-11-01 19:45:32 +0900

    20221101luyuwen

---
+++ src/co/jp/Chapter06/JavaChapter06.java
@@ -0,0 +1,44 @@
+package co.jp.Chapter06;
+
+public class JavaChapter06 {
+//	static int minus(int item1,int item2) {
+//		return item1-item2;
+//	}
+
+//	static int sum(int item1,int item2) {
+//	return item1+item2;
+//}
+//	static int sum(String item1,String item2) {
+//	return Integer.valueOf(item1)+ Integer.valueOf(item2);
+//}
+//	static int sum(int...items) {
+//	int rtn =0;
+//	for(int item:items) {
+//		rtn+=item;
+//	}
+//	return rtn;
+//}
+//	public static void main(String[] args) {
+//		int a=76;
+//		boolean flg=true;
+//		String str ="abc";
+//		System.out.println(a);
+//		System.out.println(flg);
+//		System.out.println(str);
+//		String x="1";
+//		String y="7";
+//		System.out.println(x+y);
+//		System.out.println(sum(x,y));
+//		
+//	int a =3;
+//	int b=4;
+//	int c =minus(a, b);
+//	System.out.println(c);
+//	
+//	int x =100;
+//	int y=99;
+//	System.out.println(minus(x, y));
+		// TODO 自動生成されたメソッド・スタブ
+//	}
+
+}
