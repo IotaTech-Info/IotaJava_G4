c4c1f86340b0bbe5d04cac5c6d450dc5f6942f38
zyr96 <zyr96@LAPTOP-PNH4F107>
2022-11-03 15:29:51 +0900

    homework6

---
+++ src/co/jp/chapter06/JavaChapter06.java
@@ -0,0 +1,12 @@
+package co.jp.chapter06;
+
+public class JavaChapter06 {
+
+	public static void main(String[] args) {
+
+
+		
+		
+	}
+
+}
---
+++ src/co/jp/chapter06/JavaHomeWork06.java
@@ -0,0 +1,153 @@
+package co.jp.chapter06;
+
+public class JavaHomeWork06 {
+
+	public static void main(String[] args) {
+		//question 1
+		//Answer:修飾⼦︓ public static
+		//       返却型: void
+		//       関数名: main
+		//       引数の型・名称: String ・args
+		
+		//Question 2
+		//Answer:
+		//public static int add(int[] array){
+		//  int sum = array{0};
+		 //  for (int i=0; i<array length; i++){
+		//      sum +=array[o];
+		//}
+		// return add
+		
+		//public static int Max(int[] array) {
+
+        //int Max = array[0];
+
+        //for (int i = 1; i < array.length; i++ ) {
+        //    if(Max < array[i]) {
+        //        Max = array[i];
+        //    }
+        //}
+        //return Max;
+    //}
+
+    //public static int Min(int[] array) {
+
+    //    int Min = array[0];
+
+     //   for (int i = 1; i < array.length; i++ ) {
+     //       if(Min > array[i]) {
+       //         Min = array[i];
+      //      }
+       // }
+        //return Min;
+		
+		//Question 3
+		//Answer: null
+		
+		//Question 4
+		//Answer: value2
+		
+		//Question 5
+		//Answer: true
+		
+		//Question 6-0
+		//Answer: String[]array =new array[]{"kitten kitten"];
+		//        printArray(datsaa);
+		
+		//Question 6-1
+		//Answer:  
+		//   public static void main(String args[]){
+		//    int[] left = new int[]{1,2,3,4};
+		//    int[] right = new int[]{5,6,7,8};
+		//    int[] result = mergeArray(int[]left,int[]right);
+		//    System.out.println(Arrays.toString(result));
+		//   }
+		
+		
+		//Question 6-2
+		//Answer: public static int[] subArray(int[] array, int StartIndex, int length){
+		//           int[] result =new int{length];
+		//              for(int i = startIndex; i< startIndex+length; i++){
+		//                  result[i - startIndex] =array[i];
+		//              }
+		//              return result;
+		
+		
+		
+		//Question 7
+		//Answer:
+		//public ststic char[] trim(char[] array){
+		//   int start = 0;
+		//   int over = array.length-1;
+		// for(int i =0, i<array.length; i++){
+		//    if(array[i]==(' ') continue;
+		//     start = i;
+		//   }
+		// for(int i =array.length, i >0; i--){
+		//    if(array[i]==(' ') continue;
+		//     over = i;
+		//    }
+		//  int length = over-start;
+		//  char[] result = subArray(array, start,length);
+		//  return result;
+		//}
+		    
+		   
+		//Question 8
+		//Answer:
+		//public static int[] bubblesort(int[] array){
+		
+		//   int number = 0;
+		//  for(int i = 0; i< array.length -1;i++){
+		//     for( int j = 0; j<array.length - i - 1;j++){
+		//        if(array[j] >array{j+1]){
+		//            number = array[j] +array[j+1];
+		//            array[j] =number - array[j];
+		//            array[j+1] =number - array[j];
+		//        }
+		//     }
+		//}
+		//return array;
+		
+		
+		
+		//Question 9
+		//Answer:
+		//    int[] left = new int[]{1,2,3,4};
+		//    int[] right = new int[]{5,6,7,8};
+		//   int[]result = bubblesort(mergeArray(int[]left,int[]right));
+		//   System.out.println(Arrays.toString(result));
+		//}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
+     
