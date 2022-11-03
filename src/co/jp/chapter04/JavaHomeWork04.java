dbaadf4b7af537ee437fd055b1058deec4d9b271
EB-li_biheng <114920630+Hitsune369@users.noreply.github.com>
2022-11-03 19:21:31 +0900

    Add files via upload

--- src/co/jp/chapter04/JavaHomeWork04.java
+++ src/co/jp/chapter04/JavaHomeWork04.java
@@ -3,7 +3,86 @@ package co.jp.chapter04;
 public class JavaHomeWork04 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	/*	// TODO 自動生成されたメソッド・スタブ
+		設問
+		
+		問題１︓int[] array = new int[4]; 各要素の値を記載してください。
+		
+		　　　  答え：array[0]=0 array[1]=0 array[2]=0 array[3]=0 
+		
+		
+		問題２︓int[] array = new int[] {5, 6}; 各要素の値を記載してください。
+		 
+		　　　　答え；array[0]=5, array[1]=6
+		
+		問題３︓int[][] array = new int[2][3]; 各要素の値を記載してください。
+		
+		        答え；{0,0,0} {0,0,0}
+		
+		問題４︓以下２重Foreachを理解してください。
+		
+		 int[][] array2d = new int[4][2];
+		for (int[] out : array2d) {
+		 for (int inner : out) {
+		 System.out.println(inner);
+		 }
+		}
+		
+		         答え：配列array2dの要素をすべて出力する
+		
+		問題５︓int[] array = new int[] {1, 3, 5, 7, 100, 0, 1}; 左の配列の最⼤値と最⼩桁値をSystem.out.println()。
+		
+		答え：
+			int[] array = new int[] { 1, 3, 5, 7, 100, 0, 1 };
+		int Max = array[0];
+		int Min = array[0];
+		for (int i = 1; i < array.length; i++) {
+			if (Max < array[i]) {
+				Max = array[i];
+			}
+			if (Min > array[i]) {
+				Min = array[i];
+			}
+		}
+		System.out.println("最大値は" + Max);
+		System.out.println("最小値は" + Min);
+	}
+	
+		問題６︓問題１の配列のSUMを求める。
+		
+		答え：
+			public static void main(String[] args) {
+		int x = 0;
+		int[] array = new int[4];
+		for (int i = 0; i < array.length; i++) {
 
+			x += array[i];
+
+		}
+		System.out.println(x);
+	}
+	
+		問題７︓⾏列（２次元配列）の和と積を計算してください。
+		
+		答え：		
+			public static void main(String[] args) {
+		int[][] a = { { 1, 2 }, { 3, 4 } };
+		int[][] b = { { 5, 6 }, { 7, 8 } };
+		int[][] mul = new int[2][2];
+		int[][] sum = new int[2][2];
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				mul[i][j] += a[i][j] * b[i][j];
+				sum[i][j] += a[i][j] + b[i][j];
+			}
+		}
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				System.out.println(mul[i][j]);
+				System.out.println(sum[i][j]);
+			}
+		}
+	}
+		*/
 	}
 }
