07de18adad2f39cc7c2997c67a1f0265d2a026f4
donghe <donghe@localhost>
2022-11-04 21:37:17 +0900

    homework 5

--- src/co/jp/chapter05/JavaHomeWork05.java
+++ src/co/jp/chapter05/JavaHomeWork05.java
@@ -6,4 +6,73 @@ public class JavaHomeWork05 {
 		// TODO 自動生成されたメソッド・スタブ
 
 	}
-}
+//
+	
+	1, for (int i= 1; i<101;i++) {
+		if(i%2==0) {
+			++i}
+		else if
+		   break
+		}
+	}
+
+
+2,  int sum= 1;
+    for(int=1;i<6; i++) {
+    	sum= sum * i
+    }
+    
+  
+    
+    3,int[] array= new int[] {1,6,2,7,9,6,2,1}
+    
+        for(int i = 0; i<array.length; i++) {
+        	System.out.println("現在のインデックスは " + i");"
+        	 "System.out.println(array[i]);
+        }
+    　　　
+    　　　int max =0;
+         for(int i= 0; i< array.length; i++) {
+        	 if (array[i]> max) {
+        		 max = array[i];
+        	 }
+         }
+
+         int min = 0;
+         for(int i= 0; i< array.length; i++) {
+        	 if (array[i]< min) {
+        		 min = array[i];
+        	 }
+         }
+         
+         int sum= 0
+         for(int i= 0; i< array.length; i++) {
+             sum= ++i
+                	 }
+         
+         
+          int average= sum/array.length      
+         
+         
+         4，int[] array= new int[] {1,6,2,7,9,6,2,1}
+          
+            int duplicate=0
+            for (int k=0; k<array.length; k++) 
+            for (int i=0; i<array.length; k++) {
+            	 if(k=i& array[k]!=array[i])
+            		 System.out.println(array[k])
+            }
+         
+         
+         5,  array.indexof(7)
+         
+         6,
+         7，
+         8，
+         
+         
+         
+         
+         
+
+
