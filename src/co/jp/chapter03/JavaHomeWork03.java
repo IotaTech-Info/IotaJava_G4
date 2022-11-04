1632e1a77a49cb87e38b4d39099a5e322d630ac5
mazhiqiang26 <114920794+mazhiqiang26@users.noreply.github.com>
2022-11-05 00:01:35 +0800

    馬志強の宿題3

--- src/co/jp/chapter03/JavaHomeWork03.java
+++ src/co/jp/chapter03/JavaHomeWork03.java
@@ -5,7 +5,7 @@ public class JavaChapter03 {
 	public static void main(String[] args) {
 				/*質問1:成績ランキング判断を作成する
 				 「90以上:A, 80-90:B, 70-80:C, 60-70: D, 60以下:E」*/
-		 int a = 56;
+			 int a = 56;
 			System.out.println("成績ランキング判断。入力値=" + a);
 			 if(a >= 90) {
 				 System.out.println("A");
