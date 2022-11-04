10fc77a773dc1773020f68c80f9ff81ceef20fc9
donghe <donghe@localhost>
2022-11-04 21:37:42 +0900

    06

---
+++ src/co/jp/chapter06/JavaHomeWork06.java
@@ -0,0 +1,56 @@
+package co.jp.chapter06;
+
+public class JavaHomeWork06 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+	
+	
+	1,修飾⼦：public	
+	　返却型：void
+	 　関数名： main
+	  引数（引数の型・名称）	string  args
+	  
+	  2,　public static int sum(int[] datas)
+	     for(int i= 0; i< datas.length; i++) {
+          sum= ++i
+             	 }
+	  
+}         public static int max(int[] datas)
+         int max =0;
+         for(int i= 0; i<data.length; i++) {
+	     if (data[i]> max) {
+		  max = data[i];
+		  
+		  
+		  public static int max(int[] datas)
+	         int min =0;
+	         for(int i= 0; i<data.length; i++) {
+		     if (data[i]< min) {
+			  min = data[i];
+
+			  
+			  3,null
+			  4,value1
+			  5,true
+			  6-1, public int megerArray(int[]left, int[]right)
+			        
+			     
+			   int[] result= (int[]left, int[]right)
+			   
+			   6-2, public static int subArray (int array[], startindex.length)
+			   
+			     7,public  static char trim(char[])
+			  
+			     8,
+			     9,
+			  
+			  
+			  
+			  
+			  
+			  
+			  
+			  
