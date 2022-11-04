a8243207610e0c5f532a9329821c6e79f77ef098
yuki00001 <114923004+yuki00001@users.noreply.github.com>
2022-11-05 00:17:13 +0900

    Update JavaHomeWork03.java

--- src/co/jp/chapter03/JavaHomeWork03.java
+++ src/co/jp/chapter03/JavaHomeWork03.java
@@ -4,7 +4,48 @@ public class JavaHomeWork03 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+/*
+ * 質問1:成績ランキング判断を作成する
+ * 「90以上:A, 80-90:B, 70-80:C, 60-70: D, 60以下:E」
+ * int a = 56;
+		System.out.println("成績ランキング判断。入力値=" + a);
+		if (a>=90) {
+			System.out.println("A");
+		}else if (a>=80&&a<90) {
+			System.out.println("B");
+		}else if (a>=70&&a<80) {
+			System.out.println("C");
+		}else if (a>=60&&a<70) {
+			System.out.println("D");
+		}else {
+			System.out.println("E");
+		}
+		
+質問2:日本のコインを1円、5円、10円、100円、500円があります。
+3340 円のコイン数をもとめください。
 
+        int n = 5;
+		int a[] = {500,100,10,5,1}; 
+		int sum = 3340;
+		int tes = 0;
+		int num[]= new int[n];
+		for(int i = 0,res= sum;i<n;i++) {
+			num[i] =res/a[i];
+			res-=num[i]*a[i];
+			tes+=num[i];
+				
+				
+		}
+		String s = sum + " =";
+		for(int j=0;j<n;j++) {
+			if(num[j]>0) {
+				s = s+a[j] + "*" +num[j] +"+";
+			}
+		}
+		s=s.substring(0,s.lastIndexOf("+"));
+		System.out.println("硬貨組み合わせ"+s);
+		System.out.println("枚数"+tes);
+ */
 
 	}
 }
