a3a49a3f768dbac21734c4400da7e5fa46be04cd
prfme <prfme@LAPTOP-7L9MIPB5>
2022-11-07 20:56:01 +0900

    homework06

---
+++ src/co/jp/chapter06/JavaHomeWork06.java
@@ -0,0 +1,28 @@
+package co.jp.chapter06;
+
+public class JavaHomeWork06 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		//質問１．以下関数の修飾⼦、返却型、関数名、引数（引数の型・名称）を回答してください。
+		//public staticm→修飾子 void→返却型 main→関数名(String[] args)→引数
+		
+		//質問２．配列の和、MAX値、MIN値を求める処理を関数化してください、main関数に呼び出してください。
+		
+		//難しく、問題の解き方が分かりませんでした。
+	}
+}
+			
+			
+			
+	
+	
+			
+			
+			
+			
+
+			
+	
+
