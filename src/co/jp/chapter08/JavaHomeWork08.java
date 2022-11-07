b167ea96942680e550b7cb813ed5f8f072a0888a
daisy <luyuwen.tokyo@gmail.com>
2022-11-07 20:01:44 +0900

    20221107luyuwen

---
+++ src/co/jp/chapter08/JavaHomeWork08.java
@@ -0,0 +1,65 @@
+package co.jp.chapter08;
+
+public class JavaHomeWork08 {
+	public static void main(String[] args) {
+//		質問１︓以下コードの出⼒するを回答してください。
+//		String x = "a";
+//		String y = "a";
+//		System.out.println(x == y); // 結果：true　　原因：アドレス(xとyは同じアドレス)を比較するため。
+//		System.out.println(x.equals(y)); // 結果：true 　原因：値を比較するため。
+//		String empty = "";
+//		String nullString = null;
+//		System.out.println(nullString.equals(empty)); // 結果：エラー　 原因：『null』はequalsメソッドを呼ぶことができない。
+//
+//		質問２︓以下コードの出⼒を回答してください。
+//		// Employee.java
+//		public class Employee {
+//		 private String id;
+//		 public Employee(String id) {
+//		 this.id = id;
+//		 }
+//		 @Override
+//		 public boolean equals(Object another) {
+//		 if (another instanceof Employee) {
+//		 Employee an = (Employee) another;
+//		 return an.id.equals(id);
+//		 }
+//		 return false;
+//		 }
+//		}
+//		// Kicker.java
+//		public class Kicker {
+//		 public static void main(String...args) {
+//		 Employee x = new Employee("1234");
+//		 Employee y = new Employee("1234");
+//		 System.out.println(x == y); // 結果：false 　原因：アドレス（xとyは違うアドレス）を比較するため。
+//		 System.out.println(x.equals(y)); // 結果：true 　原因：idを比較するため。
+//
+//		質問３︓以下コンパイルを修正してください。
+//		// Parent.java
+//		public class Parent {
+//		 public void hello() {
+//		 System.out.println("parent method");
+//		 }
+//		}
+//		// Child.java
+//		public class Child extends Parent {
+//		 @Override // 親クラスの強制的にOverrideする意味（アノテーション）
+//		 public void hello() {
+//		 System.out.println("child method");
+//		 }
+//		}
+//		// Kicker.java
+//		public class Kicker {
+//		 public static void print(Parent v) {
+//		 v.hello();
+//		 }
+//		 public static void main(String...args) {
+//		 print(new Parent());
+//	 print(new Child());
+//		 }
+//		}
+
+	}
+
+}
