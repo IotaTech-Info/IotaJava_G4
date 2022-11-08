e372ae98734653ed41092a1a6635a2221a8f9dba
11564 <11564@192.168.0.17>
2022-11-08 13:13:49 +0900

    homework08

--- src/co/jp/chapter08/JavaHomework08.java
+++ src/co/jp/chapter08/JavaHomework08.java
@@ -4,7 +4,13 @@ public class JavaHomework08 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		//question01:
+		//false string xとyのアドレスが違う
+		//true equalsメソッドは文字列の値を比較できる
+		
+		//question02:
+		//false string xとyのアドレスが違う
+		//true overrideされたequalsメソッドはEmployeeのインスタンスのidを比較できる
 	}
 
 }
---
+++ src/co/jp/chapter08/Question03.java
@@ -0,0 +1,10 @@
+package co.jp.chapter08;
+
+public class Question03 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
