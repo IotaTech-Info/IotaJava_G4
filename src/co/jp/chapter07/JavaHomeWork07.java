c138bec4b3f7f4650cbddbaf18fa5c95153cfecf
prfme <prfme@LAPTOP-7L9MIPB5>
2022-11-08 14:37:44 +0900

    homework07

---
+++ src/co/jp/chapter07/JavaHomeWork07.java
@@ -0,0 +1,25 @@
+package co.jp.chapter07;
+
+public class JavaHomeWork07 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//質問１︓以下コードのコンパイルエラー理由を教えてください。→petの修飾子が正しくないため
+		
+		//質問２︓Mathクラスを作成してください。以下メソッドを定義します。→分かりませんでした。
+		
+	}
+}
+			
+			
+			
+	
+	
+			
+			
+			
+			
+
+			
+	
+
