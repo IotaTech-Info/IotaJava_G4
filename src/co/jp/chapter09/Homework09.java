2640dfc2af24467c8ed6896f4e492b2c42806393
11564 <11564@192.168.0.17>
2022-11-10 19:26:53 +0900

    homework09のコミット

--- src/co/jp/chapter09/Homework09.java
+++ src/co/jp/chapter09/Homework09.java
@@ -4,6 +4,8 @@ public class Homework09 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		//question01: ラムダ式はbtn.addActionListenerというメソッドの引数になる．
+		//question02:　LINE６ー８
 
 	}
 
