839cfd614e0a0219f5885256d9632fb04ad5a594
zyr96 <zyr96@LAPTOP-PNH4F107>
2022-11-11 14:47:33 +0900

    homework08

--- src/co/jp/chapter08/JavaHomeWork08.java
+++ src/co/jp/chapter08/JavaHomeWork08.java
@@ -7,11 +7,16 @@ public class JavaHomeWork08 {
 	
 	public static void main(String[] args) {
 	//質問１
-	//回答：3．ture 
-	//	    4. ture
-	//	    8. false
+	//回答：3．ture 字符串地址相同
+	//	    4. ture 字符串内容相同 
+	//	    8. false 字符串内容地址都不相同
 	//質問２
-	//回答：
+	//回答：21.false  字符串地址不同
+	//	    22. ture 字符串的内容相同
+		
+		
+	//質問３
+    //回答：16. public static void print(parent v) {		
 	}
 
 }
