e2ac4c0291fca7d25523d63488e179eb76f30e99
zyr96 <zyr96@LAPTOP-PNH4F107>
2022-11-11 14:46:25 +0900

    homework8

---
+++ src/co/jp/chapter08/JavaHomeWork08.java
@@ -0,0 +1,17 @@
+package co.jp.chapter08;
+
+public class JavaHomeWork08 {
+
+	static int num;
+
+	
+	public static void main(String[] args) {
+	//質問１
+	//回答：3．ture 
+	//	    4. ture
+	//	    8. false
+	//質問２
+	//回答：
+	}
+
+}
