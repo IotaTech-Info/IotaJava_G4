6745a78b855edc273c295a6c280d76432ee4ac3d
donghe <donghe@localhost>
2022-11-12 16:51:09 +0900

    homework08

---
+++ src/co/jp/chapter08/Homework08.java
@@ -0,0 +1,46 @@
+package co.jp.chapter08;
+
+public class Homework08 {
+
+/**
+ 1， String x = "a";
+String y = "a";
+System.out.println(x == y); 
+// true  两者的地址值一样
+System.out.println(x.equals(y)); 
+ true 两者的内容一样
+String empty = "";
+String nullString = null;
+System.out.println(nullString.equals(empty)); 
+// false内容不同
+ * 
+ 
+ 
+ 2.
+ ①false，地址值不同
+ 2　true，内容相同
+ 
+ 3，
+ 把 child v 变成parent v 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ * 
+ */
+
+
+
+
+
+
+
+
+
+
+
+}
