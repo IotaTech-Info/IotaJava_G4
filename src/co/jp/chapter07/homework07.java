8d2df1ebde270fb741a56d2e53e6b23f3b5f9406
donghe <donghe@localhost>
2022-11-12 16:50:45 +0900

    homework07

---
+++ src/co/jp/chapter07/homework07.java
@@ -0,0 +1,52 @@
+package co.jp.chapter07;
+
+	/**
+	 * 问题1：
+	  
+	   因为pet.java里面没有age的方法
+	
+	2，
+	public class Math{
+	      public static int max(int [] input){
+	             int max = input[0];
+	             for(int i = 0; i < input.length; i++){
+	                      if (max< input[i]){
+	                      max= input[i];
+	                      }
+	             }
+	             return max;
+}
+
+
+       public static int min(int [] input){
+	             int min = input[0];
+	             for(int i = 0; i < input.length; i++){
+	                      if (min>input[i]){
+	                      min= input[i];
+	                      }
+	             }
+	             return mim;
+	             
+	             
+	      public static double ave(int [] input){
+	             int sum= 0;
+	             for(int i = 0; i < input.length; i++){
+	                      sum= ++input[i];
+	                      }
+	             double average= sum/ input.length
+	             return average;
+                  }
+          
+          public class Array{
+          public static void main(string[] args){
+                  int[] array= new int[]{1,2,3,4};
+                  int arraymax=max(array)
+                  int arraymin=min(array)
+                  int arrayaverage=ave(array)
+          
+          
+          2，不太会
+          
+
+	
+	*/
