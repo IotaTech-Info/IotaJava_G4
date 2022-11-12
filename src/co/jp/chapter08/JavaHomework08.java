ea3f823c1cb7617e568eaa9c472a842ac4535f9b
ZHANG Boyang <zhanginjp@srmtlab.org>
2022-11-13 00:15:11 +0900

    Update JavaHomework08.java

--- src/co/jp/chapter08/JavaHomework08.java
+++ src/co/jp/chapter08/JavaHomework08.java
@@ -2,4 +2,82 @@ package co.jp.chapter08;
 
 public class JavaHomework08 {
 
+	public static void main(String[] args) {
+//	質問１︓以下コードの出⼒するを回答してください。
+//	
+//	String x = "a";
+//	String y = "a";
+//	System.out.println(x == y); // true ? false? 原因は ?　回答：true
+//	System.out.println(x.equals(y)); // true ? false? 原因は ?　回答：true
+//
+//	String empty = "";
+//	String nullString = null;
+//	System.out.println(nullString.equals(empty)); // true ? false? 原因は ? 回答：無出力、エラー：NullPointerException
+//	
+
+//	質問２︓以下コードの出⼒を回答してください。
+//
+//	// Employee.java
+//	public class Employee {
+
+//		private String id;
+	
+//		public Employee(String id) {
+//			this.id = id;
+//		}
+	
+//		@Override
+//		public boolean equals(Object another) {
+//			if (another instanceof Employee) {
+//				Employee an = (Employee) another;
+//				return an.id.equals(id);
+//			}
+//			return false;
+//		}
+//	}
+	
+//	// Kicker.java
+//	public class Kicker {
+
+//		public static void main(String...args) {
+//			Employee x = new Employee("1234");
+//			Employee y = new Employee("1234");
+//			System.out.println(x == y); // true ? false? 原因は ?　回答：false
+//			System.out.println(x.equals(y)); // true ? false? 原因は ?　回答：true
+//		}
+	
+//	}
+//	
+//	質問３︓以下コンパイルを修正してください。
+//	// Parent.java
+//	public class Parent {
+
+//		public void hello() {
+//			System.out.println("parent method");
+//		}
+//	}
+	
+//	// Child.java
+//	public class Child extends Parent {
+
+//		@Override // 親クラスの強制的にOverrideする意味（アノテーション）
+//		public void hello() {
+//			System.out.println("child method");
+//		}
+//	}
+	
+//	// Kicker.java
+//	public class Kicker {
+//		public static void print(Child v) { 
+//			v.hello();
+//		}
+	
+//		public static void main(String...args) {
+//			print(new Parent());
+//			print(new Child());
+//		}
+//	}
+//	回答：print()の引数はParent vにするべきです	
+	
+	}
 }
