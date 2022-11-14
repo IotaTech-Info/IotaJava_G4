ef54ace19bd0e22b97184b8cc2d913aa57239e8f
liuxiaoyang98 <liuxiaoyang98@gmail.com>
2022-11-14 19:38:18 +0900

    Update JavaHomeWork07.java
    質問07

--- src/co/jp/chapter07/JavaHomeWork07.java
+++ src/co/jp/chapter07/JavaHomeWork07.java
@@ -1,5 +1,55 @@
 package co.jp.chapter07;
 
 public class JavaHomeWork07 {
+	/*
+// Pet.java
+ public class Pet {
+ public String name;
+
+ public void setName(String value) {
+ this.name = value;
+ }
+}
+// Kicker.java
+ public class Kicker {
+ public static void main(String...args) {
+ Pet cat = new Pet();
+ cat.setAge(1234);
+ }
+}
 	
+	*/
+
+//コンパイルエラーはPetのクラスの中に「setAge」のメソッドがない。
+	
+	public class Math(){
+		
+		
+		
+		
+		public static int MAX(int[] input){
+			int max=input[0];
+			for(int i: input){
+				if(max<i)max=i
+			}
+			return max;
+		}
+		public static int MIN(int[] input){
+			int min=input[0];
+			for(int i: input){
+				if(min>i)min=i
+			}
+			return min;
+		}
+		public static double avg(int[] input){
+			int sum=0;
+			int len=input.length();
+			for (int i=0;i<len;i++){
+				sum=sum+input[i];	
+			}
+			return (1.0*sum)/len;
+		}
+		
+		
+	}
 }
