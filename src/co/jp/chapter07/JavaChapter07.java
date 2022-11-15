83d34bb62f135fe4f83c85aae69c5e4b30da77ab
wangsy <wangsy@localhost>
2022-11-16 00:36:53 +0900

    quiz

--- src/co/jp/chapter07/JavaChapter07.java
+++ src/co/jp/chapter07/JavaChapter07.java
@@ -4,7 +4,8 @@ package co.jp.chapter07;
 public class JavaChapter07 {
 
 	public static void main(String[] args) {
-		
+		Pet pet = new Pet("kitty", 2, "www");
+		System.out.println(pet.getZhurenname());
 		
 	}
 
--- src/co/jp/chapter07/Pet.java
+++ src/co/jp/chapter07/Pet.java
@@ -1,10 +1,29 @@
 package co.jp.chapter07;
 
 public class Pet {
-	public String name;
+	private final String name;
+	private final int age;
+	private final String zhurenname;
 	
-	public void setName(String value) {
-		this.name = value;
+	public Pet(String name, int age, String zhurenname) {
+		this.name = name;
+		this.age = age;
+		this.zhurenname = zhurenname;
+		
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getZhurenname() {
+		return zhurenname;
+	}
+	
+	
 
 }
