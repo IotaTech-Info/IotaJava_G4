2866f679d7afda27e51cb194894160ab8860fe1e
zyr96 <zyr96@172.16.53.48>
2022-11-16 14:46:55 +0900

    homework09

---
+++ src/co/jp/chapter09/JavaHomeWork09.java
@@ -0,0 +1,140 @@
+package co.jp.chapter09;
+
+public class JavaHomeWork09 {
+
+
+	public static void main(String[] args) {
+
+   //質問１
+   //回答
+   //	IPet cat の関数のインタフェースを実装する。
+		
+   //質問２
+   //回答    
+		
+//		匿名クラス
+//		new Thread() {
+//		 @Override
+//		 public void run() {
+//		 System.out.println("thread running...");		
+		
+		
+//		練習問題
+//		質問１︓クラス（Class）とは︖クラス定義⽤のキーワードは「 class 」である。
+//		
+		
+//		質問２︓クラスメンバ（Class member）の何種類ありますか。
+//		1. 属性・フィールド (Field)
+//		2. 関数(Function)・メソッド(Method)・操作(Operator)
+//		
+//		
+//		質問３︓クラスメンバの関数は特殊の関数「コンストラクタ」が持っています。
+//		
+//		
+//		質問４︓コンストラクタの特徴
+//		1. 戻り値不要
+//		2. 関数名＝クラス名称
+//		
+//		
+//		質問５︓コンストラクタの作⽤︓クラス属性の初期化する。
+//		
+//		
+//		質問６︓１つクラスに複数のコンストラクタ持つ可能。
+//		
+//		
+//		質問７︓キーワード this の使⽤⽅法︓ this.name ︖ name ?
+//		
+//				
+//		質問８︓クラスを利⽤する為、インスタンス化する必要です。インスタンスの演算⼦は
+//				new です。
+//		
+//		
+//		質問９︓演算⼦ new の直後呼び出しているメソッドは「コンストラクタ」︔
+//		
+//		
+//		質問１０︓アクセス制御の３種類あります。
+//				1. + public
+//				2. # protected
+//				3. - private
+//		
+//		
+//		質問１１︓静的のメンバの定義するキーワード（修飾⼦）は static 。
+		
+		
+//		質問１２：	静的メンバアクセス時、インスタンス化不要。アクセス方法は「クラス名.静的のメンバ	].
+		
+//		
+//		質問１３；
+//
+//		public class Animal{
+//		 int age;
+//		 String name,type;
+//		 
+//		 public Animals(int age, String name,String type) {
+//				super();
+//				
+//				this.age = age;
+//				this.name = name;
+//				this.type = type;
+//	}		
+//		 void speck;{
+//			 System.out.println("hello");
+//		 }
+//		 
+//
+//		void getName() {
+//			
+//				System.out.println(name);
+//			}
+//	
+		
+//      質問１４：猫の名前を出力する。
+		
+//		質問１５：クラス継承する⽤キーワードは「 extends 」
+		
+//　　　質問１６：継承しているクラス。上位クラスは「親クラス」・「スーパークラス」。下位クラスは「サブクラス」と呼びます。
+		
+//		質問１７︓サブクラス＆アクセス制御（ public, protected ）。
+		
+//		質問１８︓サブクラスと親クラス同じメソッド名かつメソッドの引数は⼀致する場合、「オーバーライド」となります。
+
+//		質問１９：
+//		public class Cat extends Animal{
+//			void sayHello() {
+//				System.out.println("cat is saying hello");
+//			}
+//
+//
+//			@Override
+//			void speck() {
+////				super.speck();
+//				System.out.println("ニャー");
+//				super.speck();
+//			}
+//		}
+//		public class Dog extends Animal{
+//			void sayHello() {
+//				System.out.println("dog is saying hello");
+//			}
+//
+//
+//			@Override
+//			void speck() {
+////				super.speck();
+//				System.out.println("ワン");
+//				super.speck();
+//			}
+
+//		質問２０：
+		
+//		1.Animal v0 = new Animal(); // v0 は Animal 型
+//		2.Animal v1 = new Cat("a cat"); // v1 は Cat 型, v1 は Animal 型
+//		3.Animal v2 = new Dog("a dog"); // v2 は Dog 型, v2 は Animal 型
+//		4.Cat v3 = new Cat(""); // OK
+//		5.Dog v4 = new Dog(""); // OK
+//		6.Cat v5 = new Animal(""); // NG
+		
+		
+	}
+}
+
