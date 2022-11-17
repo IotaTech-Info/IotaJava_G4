1db61ac5ea448899aeb5de31fc44a9d6ed879e76
dzw <dzw@localhost>
2022-11-17 15:49:17 +0900

    HomwWork_01

--- src/co/jp/chapter09_1/HomeWork09_1.java
+++ src/co/jp/chapter09_1/HomeWork09_1.java
@@ -1,5 +1,53 @@
 package co.jp.chapter09_1;
 
 public class HomeWork09_1 {
+//	質問１︓以下ソースにラムダ式の使う場所を解釈してください。
+//
+//	import java.awt.event.ActionEvent;
+//	 import java.awt.event.ActionListener; 
+//	import javax.swing.JButton; 
+//	import javax.swing.JFrame; 
+//
+//	public class GUISample {
+//
+//	 public static void main(String[] args) { 
+//	JFrame window = new JFrame("DCNet Java 教育"); 
+//	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+//	window.setSize(800, 600); 
+//	JButton btn = new JButton("hello world"); 
+//	window.getContentPane().add(btn); 
+//
+//
+//	btn.addActionListener((ActionEvent e) -> {
+//	 System.out.println("ボタンクリックしました。"); 
+//
+//	});
+//
+//
+//	ラムダ式の使われる場所：
+//	btn.addActionListener((ActionEvent e) -> {
+//	 System.out.println("ボタンクリックしました。"); 
+	//省略了接口名和方法名
+//
+//	});
+//
+//
+//
+//	質問２︓以下ソースに匿名クラスの利⽤する⾏は︖
+//	public class Kicker { 
+//
+//	public static void main(String... args) { 
+//	new Thread() {
+//	 @Override
+//	 public void run() { 
+//	System.out.println("thread running..."); 
+//	}
+//	 }.start(); 
+//	}
+//	 }
+//
+//	以下ソースに匿名クラスの利⽤する⾏は:
+//	new Thread()
+//
 
 }
