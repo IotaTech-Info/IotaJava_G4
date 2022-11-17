0207c0343cc7a82d0962a197ca49013d0c6d0f31
Jo Sawakai <98514826+SumizomeShizuku@users.noreply.github.com>
2022-11-17 22:38:04 +0900

    HomeWork09

---
+++ src/co/jp/chapter09/JavaHomeWork09.java
@@ -0,0 +1,17 @@
+package co.jp.chapter09;
+
+public class JavaHomeWork09 {
+
+	static int num;
+
+	public static void main(String[] args) {
+
+//質問 1
+//		省略了btn.addActiongListener里的接口名和方法名，并省略了e的参数类型
+		
+//質問 2
+//		第4行 
+		
+	}
+
+}
