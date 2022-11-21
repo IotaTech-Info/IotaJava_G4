40e1095599bf9f1633a9a0f134247b6faa05378b
liuxiaoyang98 <liuxiaoyang98@gmail.com>
2022-11-21 10:37:11 +0900

    宿題07の提出

--- src/co/jp/chapter07/JavaHomeWork07.java
+++ src/co/jp/chapter07/JavaHomeWork07.java
@@ -21,35 +21,68 @@ public class JavaHomeWork07 {
 	*/
 
 //コンパイルエラーはPetのクラスの中に「setAge」のメソッドがない。
-	
-	public class Math(){
-		
-		
-		
-		
+
+	public class Math{
+
+
+
+
 		public static int MAX(int[] input){
 			int max=input[0];
 			for(int i: input){
-				if(max<i)max=i
+				if(max<i)max=i;
 			}
 			return max;
 		}
 		public static int MIN(int[] input){
 			int min=input[0];
 			for(int i: input){
-				if(min>i)min=i
+				if(min>i)min=i;
 			}
 			return min;
 		}
 		public static double avg(int[] input){
 			int sum=0;
-			int len=input.length();
+			int len=input.length;
 			for (int i=0;i<len;i++){
 				sum=sum+input[i];	
 			}
 			return (1.0*sum)/len;
 		}
-		
-		
+	
 	}
+	public class Matrix {
+		private final int[][] data;
+
+		public Matrix(int[][] data) {
+			this.data=data;
+		}
+		public int w(){
+			return data.length;
+		}
+		public int h(){
+			return data[0].length;
+		}
+		public int[][] display(){
+			return data;
+		}
+		public Matrix add(Matrix b) {
+			if(!(b.w() == this.w() && b.h() == this.h())) {
+				return null;
+			}
+			int[][] b_data=b.display();
+			int[][] ret_data=new int[b.w()][b.h()];
+			int[] tempw=new int[b.h()];
+			int temph;
+			for(int w=0;w<b.w();w++) {
+				for(int h=0;h<b.h();h++) {
+					temph=data[w][h]+b_data[w][h];
+					tempw[h]=temph;
+				}
+				ret_data[w]=tempw;
+			}
+			return new Matrix(ret_data);
+		}
+	}
+
 }
