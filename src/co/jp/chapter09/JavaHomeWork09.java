8ea9f2f24e767ea1603d0276549a0ac9b7a444b2
liuxiaoyang98 <liuxiaoyang98@gmail.com>
2022-11-21 10:01:07 +0900

    宿題09の提出

--- src/co/jp/chapter09/JavaHomeWork09.java
+++ src/co/jp/chapter09/JavaHomeWork09.java
@@ -1,4 +1,23 @@
 package co.jp.chapter09;
 
 public class JavaHomeWork09 {
+
+	public static void main(String[] args) {
+/*
+ * 
+ * 
+btn.addActionListener((ActionEvent e) -> {// JButtonクラスの btn のメソッドをOverrideする
+System.out.println("ボタンクリックしました。");
+});
+ */
+		
+		new Thread() {//この行で匿名クラスを作った 4行目
+			@Override
+			public void run() {
+				System.out.println("Thread is running...");
+			}
+			
+		}.start();
+	}
+
 }
