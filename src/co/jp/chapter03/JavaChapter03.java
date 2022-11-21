af75c07d6f43f68a4458d7aea11eb68d1ae929b6
21q2106 <clfj216@gmail.com>
2022-11-21 18:40:25 +0900

    part2中间发表

--- src/co/jp/chapter03/JavaChapter03.java
+++ src/co/jp/chapter03/JavaChapter03.java
@@ -4,6 +4,105 @@ public class JavaChapter03 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+//if else
+//		int a=50;
+//		int b=100;
+//		if(a==b) {
+//			System.out.println("a=b");
+//		} 
+//		else {
+//			System.out.println("a!=b");
+//		}
+		
+		//
+		//int age=100;
+		//if(age<=6) {
+			//System.out.println("幼児,無料");
+		//}
+		//else if(age>6 && age<=12) {
+			//System.out.println("小学生,100円");
+		//}
+		//else if(age>12 && age<=18) {
+			//System.out.println("中高生，500円");
+		//}
+		//else if(age>18) {
+			//System.out.println("大人，1000円");
+		//}
+		
+		//
+//		String sex="女性";
+//		int age=10;
+//		if("男性".equals(sex)) {
+//			if(age>18) {
+//				System.out.println("man");
+//			}
+//			else {
+//				System.out.println("boy");
+//			}
+//		}
+//		else{
+//			if(age>18) {
+//				System.out.println("woman");
+//			}
+//			else {
+//				System.out.println("girl");
+//			}
+//		}
+		
+		
+		//loop for
+//		int c=0;
+//		System.out.println("loop start");
+//		for(;;) {
+//			System.out.println(c);
+//			c++;
+//			break;
+//		}
+//			System.out.println("loop end");
+		
+		
+//		System.out.println("loop start");
+//		for(int a=0;a<=11;a++) {
+//			System.out.println(a);
+	//	}
+//		for(int a=10;a>=0;a--) {
+//			System.out.println(a);
+//		}
+//		System.out.println("loop end");
+		
+		
+		System.out.println("loop start");
+		for(int a=0;a<10;a++) {
+			for(int b=0;b<10;b++) {
+				System.out.println("a="+a+":b="+b);
+				
+				if(a == b || a+b==9) {
+				System.out.print("* ");
+				}
+				else {
+					System.out.print("   ");
+				
+				}
+				}
+			
+			System.out.println();
+		}
+		
+//		int ent=100;
+//		for(int a=0;a<ent;a++) {
+//			if(a%2>0) {
+//				continue;
+//			}
+//			System.out.print(a+",");
+//			if(a==50) {
+//				break;
+//			}
+		}
+//		System.out.println();
+			}
+		
+//	}
 
-	}
-}
+
+	
+
