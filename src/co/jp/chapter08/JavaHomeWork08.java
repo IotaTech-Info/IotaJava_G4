a4b511bf9df8e4900220c4bf20dfe13f93ecd152
liuxiaoyang98 <liuxiaoyang98@gmail.com>
2022-11-21 10:01:49 +0900

    宿題08の提出

--- src/co/jp/chapter08/JavaHomeWork08.java
+++ src/co/jp/chapter08/JavaHomeWork08.java
@@ -1,5 +1,27 @@
 package co.jp.chapter08;
 
 public class JavaHomeWork08 {
+
+	@SuppressWarnings("null")
+	public static void main(String[] args) {
+
+		/*
+		 * 質問1:以下コードの出力するを回答してください。
+*/
+	String x="a";
+	String y="a";
+	System.out.println(x == y); // true ですけど　falseが正しい出力と思っている(x と y のアドレスが違う)
+	System.out.println(x.equals(y)); // true
+	String empty="";
+	String nullString = null;
+	System.out.println(nullString.equals(empty));//Errorが出す
 	
+	/*質問2:以下コードの出力を回答してください。
+	 21行の出力:false (x と y のアドレスが違う)
+	 22行の出力:true (equalsのメソッドをOverrideしたので)
+	*/
+	
+
+	}
+
 }
