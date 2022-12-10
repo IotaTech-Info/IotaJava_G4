222cd02ca864fbd87c36ba4aa1e6a6b52e066d65
DDG-Edgerunners <jyzhou970402@outlook.com>
2022-12-10 22:27:44 +0900

    09

--- src/co/jp/chapter08/JavaHomeWork08.java
+++ src/co/jp/chapter08/JavaHomeWork08.java
@@ -2,22 +2,82 @@ package co.jp.chapter08;
 
 public class JavaHomeWork08 {
 
-
 	public static void main(String[] args) {
-        // 
-	// 質問１
-//		String x = "a";
-//		String y = "a";
-//		System.out.println(x==y);   true "a"的地址是固定的，指针把a的地址给了x和y 所以比较地址时值是一样的。
-//		System.out.println(x.equals(y)); true  x和y的值都是"a"因此相等 true
-//		String empty = "";
-//		String nullString = null;
-//		System.out.println(empty.equals(nullString)); false 两个值不同。
-	// 質問２	
-//	false
-//	true
+		// TODO 自動生成されたメソッド・スタブ
 		
+		// 質問1：以下コードの出力を回答してください。
+		String x = "a";
+		String y = "a";
 		
+		System.out.println(x == y);
+		// 输出：true; 
+		
+		System.out.println(x.equals(y));
+		// 输出：true; 
+		
+		String empty = "";
+		String nullString = null;
+		System.out.println(empty.equals(nullString));
+		// 输出：false; 
+		
+		// 質問2：以下コードの出力を回答してください。		
+		//	Employee.java
+		//	public class Employee {
+		//		private String  id;
+		//		public Employee(String id) {
+		//			this.id = id;
+		//		}
+		//		@Override
+		//		public boolean equals(Object another) {
+		//			if(another instanceof Employee) {
+		//				Employee an = (Employee)another;
+		//				return an.id.equals(id);
+		//			}
+		//			return false;
+		//		}
+		//	}
+		// 	Kicker.java
+		//	public class Kicker{
+		//		public static void main(String[] args){
+		//			Employee x = new Employee("1234");
+		//			Employee y = new Employee("1234");
+		//			System.out.println(x == y);
+		// 输出：false; 
+		//			System.out.println(x.equals(y));
+		// 输出：true; 
+		
+		// 質問3：以下コンパイルを修正してください。
+		// 修正的代码:
+		// 	Parent.java
+		//	public class Parent{
+		//		public void hello(){
+		//			System.out.println("paretn method");
+		//		}
+		//	}
+		//	Child.java
+		//	public class Child extends Parent{
+		//		@Override
+		//		public void hello(){
+		//			System.out.println("child method");
+		//		}
+		//	}
+		//	Kicker.java
+		//	public class Kicker{
+		//		public static void print(Child v){
+		//			v.hello();
+		//		}
+		//		public static void main(String[] args){
+		//			new Parent().hello();  // 修正的部分
+		//			print(new Child());
+		//
+		//			错误原因：父类不能调用子类重写方法
+		//
+		//			修正方法：将print(new Parent());改为new Parent().hello();
+		//			或者Parent p = new Parent(); p.hello();			
+		//
+		//			输出：new Parent().hello()只能调用到父类Parent的hello方法，输出为parent method；
+		//			print(new Child());调用子类Child的hello方法，输出为child method。
+		//		}
+		//	}
 	}
-
 }
--- src/co/jp/chapter09/JavaHomeWork09.java
+++ src/co/jp/chapter09/JavaHomeWork09.java
@@ -2,13 +2,6 @@ package co.jp.chapter09;
 
 public class JavaHomeWork09 {
 
-	static int num;
-
-	public static void main(String[] args) {
-		Cat cat = new Cat();
-		cat.name = "neko";
-		System.out.println(cat.name);
-		cat.speck();
 
 	}
 
