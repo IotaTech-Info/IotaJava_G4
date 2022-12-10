17d3ab242fa19d1312c962fb63ee1c7e3c1e2660
cuiguangpuxiu <cgpx891@gmail.com>
2022-12-10 18:50:01 +0900

    宿題08

---
+++ src/co/jp/chapter08/JavaChapter08.java
@@ -0,0 +1,40 @@
+package co.jp.chapter08;
+
+public class JavaChapter08 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+	
+		
+		Animal an = new Animal("动物",5,4);
+		
+		an.name = "动物";
+		//System.out.println(an.name);
+		an.speak();
+		
+		
+		
+		Cat cat = new Cat("hello kiity",5,4,"fuafua");
+		
+		cat.name = "hello kiity";
+		//System.out.println(cat.name);
+		cat.speak();
+		
+		
+		//Bird bird = new Bird("niao",1,2,"feifei");
+		
+	
+		
+		//String x = "a";
+		//String y = "a";
+		//System.out.println(x.equals(y)); 
+		
+		//String empty = "";
+		//String nullString = null;
+		//System.out.println(nullString.equals(empty));
+		
+		
+	}
+	
+
+}
