6ec879c7761cf6a5e345b02cce541c050565ea1b
cuiguangpuxiu <cgpx891@gmail.com>
2022-12-10 18:49:26 +0900

    宿題07

---
+++ src/co/jp/chapter07/JavaHomeWork07.java
@@ -0,0 +1,75 @@
+package co.jp.chapter07;
+
+public class JavaHomeWork07 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		
+		//質問１︓以下コードのコンパイルエラー理由を教えてください。
+		//1 // Pet.java
+		//2 public class Pet {
+		//3 	 public String name;
+        //4
+		//5 	 public void setName(String value) {
+		//6 	 this.name = value;
+		//7 	 }
+		//8 	}
+		//9 	// Kicker.java
+		//10 	public class Kicker {
+		//11 	 public static void main(String...args) {
+		//12 	 Pet cat = new Pet();
+		//13 	 cat.setAge(1234);
+		//14 	 }
+		//15 	}
+
+		
+		//回答：PetにsetAgeのメソッドがないです
+		
+		
+		
+		
+		
+		
+		
+		//質問２︓Mathクラスを作成してください。以下メソッドを定義します。
+		//1. 配列のMAXを求める静的メソッドを定義。（引数は int 配列）
+		//2. 配列のMINを求める静的メソッドを定義。（引数は int 配列）
+		//3. 配列の平均値を求める静的メソッドを定義。（引数は int 配列）
+		//4. mainメソッドで上記それぞれメソッドを呼び出してください。
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
