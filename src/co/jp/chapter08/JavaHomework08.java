07ff3ba2042d626a17a572529a449b6d76cb14de
LAW <LAW@DESKTOP-V60M247>
2022-12-15 12:27:37 +0800

    宿題

--- src/co/jp/chapter08/JavaHomework08.java
+++ src/co/jp/chapter08/JavaHomework08.java
@@ -3,8 +3,37 @@ package co.jp.chapter08;
 public class JavaHomework08 {
 	public static void main(String[] args) {
 		
+		/*質問１
+	
+		String x = "a";
+		String y = "a";
+		System.out.println(x == y); // true  xとýのJVMにあるアドレスを比較するため。
+		System.out.println(x.equals(y)); // true xとýの値を比較するため。
+		String empty = "";
+		String nullString = null;
+		System.out.println(nullString.equals(empty)); 
+		// エラー 定義されていない変数の属性あるいはその変数を含むメゾットを引用することができないため。
+		*/
 		
+		/*質問２
 		
+		System.out.println(x == y); //false アドレスが異なるため
+ 		System.out.println(x.equals(y)); // true  IDは同じため。
+		//
+		*/
 		
+		/*質問３
+	
+	public static void print(Parent v) {
+		 v.hello();
+	 }
+	
+	public static void main(String...args) {
+		 
+		print(new Parent());
+		print(new Child());
+		
+	}
+		*/
 	}
 }
