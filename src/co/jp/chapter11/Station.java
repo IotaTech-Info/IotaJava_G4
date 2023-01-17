87b0e83483bf476819fed6c7dd41d7394d01d065
zyr96 <zyr96@LAPTOP-PNH4F107>
2023-01-18 01:46:52 +0900

    homework11

---
+++ src/co/jp/chapter11/Station.java
@@ -0,0 +1,17 @@
+package co.jp.chapter11;
+
+public class Station {
+	
+	public String name;
+	public int number;
+	
+	
+	public Station(String name, int number) {
+		super();
+		this.name = name;
+		this.number = number;
+	}
+	
+	
+
+}
