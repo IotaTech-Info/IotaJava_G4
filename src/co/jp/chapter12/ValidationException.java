7c7a23c1eaf36480a741f666f6afc42634601c22
LAW <LAW@DESKTOP-V60M247>
2023-01-29 16:28:19 +0800

    宿題

---
+++ src/co/jp/chapter12/ValidationException.java
@@ -0,0 +1,17 @@
+package co.jp.chapter12;
+
+public class ValidationException extends Exception{
+	
+	private String name; // カラム名称
+	private String message; // エラーメッセージ
+	
+	public ValidationException(String name, String message) {
+		this.name = name;
+		this.message = message;
+	}
+	
+	void error() {
+		System.out.println(name + message);
+	}
+
+}
---
+++ src/co/jp/chapter13/chapter13homework.java
@@ -0,0 +1,42 @@
+package co.jp.chapter13;
+
+public class chapter13homework {
+	/*
+	public static void main(String[] args) {
+		
+	
+		 Logger logger = Logger.getLogger("text1");
+		 
+		 logger.severe("severe");
+		 logger.warning("warning");
+		 logger.info("info");
+		 
+		 logger.config("config setting");
+		 logger.fine("fine");
+		 logger.finer("finer");
+		 logger.finest("finest");
+	
+	}
+	*/
+	
+	/*
+	public static boolean writeDataToCsv(String path, String data) {
+		 File file = new File(path);
+		if (!file.exists()) {
+			return false;
+		}
+			
+		try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file,true))){
+			bufferedWriter.write(data);
+			bufferedWriter.newLine();
+			bufferedWriter.flush();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		return true;
+		
+
+	}
+	*/
+
+}
