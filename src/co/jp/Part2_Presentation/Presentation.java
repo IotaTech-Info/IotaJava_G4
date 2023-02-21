dac3406b76ab88659f7b40fabb69af045ccb5b0d
Dolly <Dolly@DESKTOP-30A8GE6>
2023-02-21 13:40:27 +0900

    HTML宿題

--- src/co/jp/Part2_Presentation/Presentation.java
+++ src/co/jp/Part2_Presentation/Presentation.java
@@ -13,63 +13,52 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Presentation {
-		
-	static Pet[] hotel = new Pet[10];
-	
 
-	
+	static Pet[] hotel = new Pet[10];
+
 	public static void main(String[] args) {
-	
-			 
-		 Dog myDog = new Dog();
-		 Cat myCat = new Cat();
-		 
-		 
+
+		Dog myDog = new Dog();
+		Cat myCat = new Cat();
+
 		Date currentDate = new Date();
 		Calendar c = Calendar.getInstance();
 		c.setTime(currentDate);
-		
+
 		List<String> vip = new ArrayList<>();
-		 
+
 //		 String name = "";
 //		 int age = 0;
 //		 String type = "";
-		
-		 Scanner sc_1 = new Scanner(System.in);
-		 Scanner sc_2 = new Scanner(System.in);
-		 Scanner sc_3 = new Scanner(System.in);
-		
-		
+
+		Scanner sc_1 = new Scanner(System.in);
+		Scanner sc_2 = new Scanner(System.in);
+		Scanner sc_3 = new Scanner(System.in);
+
 		System.out.println("----- いらっしゃいませ。ペットホテルへようこそ。現在の空室状況は -----");
 		System.out.println("hotel: " + Arrays.toString(hotel));
-		
+
 		for (;;) {
-		
 
 			System.out.println("---ループ開始。0は空室チェック、１はチェックイン、２は給食、３は他サービス、４はチェックアウト、5は意見を書く、6会員になる、7は退出");
 			int input_fn = sc_1.nextInt();
 
-
 			// room check
-			if(input_fn ==0) {
+			if (input_fn == 0) {
 				int i;
-				for (i = 0; i < hotel.length-1; i++)
-					{
+				for (i = 0; i < hotel.length - 1; i++) {
 					if (hotel[i] != null) {
 						System.out.println(hotel[i].name + "," + hotel[i].type);
-					} 
-				else {
+					} else {
 						System.out.println("空室");
 					}
 
 				}
 
 			}
-			
-			
 
 			// check in
-			else if (input_fn ==1) {
+			else if (input_fn == 1) {
 				System.out.println("---名前、年齢、タイプを入力してください。---");
 				System.out.println("まずはペットの名前（例：Snoopy）");
 				String name = sc_2.nextLine();
@@ -80,29 +69,27 @@ public class Presentation {
 				if (type.equals("DOG")) {
 					myDog.type = "DOG";
 					myDog.name = name;
-					myDog.age = age;			
+					myDog.age = age;
 					System.out.println("1から10までの部屋から選べます。入力してください。");
 					String input_room = sc_2.nextLine();
 					Pet myPet = myDog;
-					hotel[Integer.valueOf(input_room) - 1] = myPet;	
-					}
-				else if (type.equals("CAT")) {
+					hotel[Integer.valueOf(input_room) - 1] = myPet;
+				} else if (type.equals("CAT")) {
 					myCat.type = "CAT";
 					myCat.name = name;
-					myCat.age = age;					
+					myCat.age = age;
 					System.out.println("1から10までの部屋から選べます。入力してください。");
 					String input_room = sc_2.nextLine();
 					Pet myPet = myCat;
 					hotel[Integer.valueOf(input_room) - 1] = myPet;
 				}
 				System.out.println("チェックイン完了。");
-				//Print Room Information
+				// Print Room Information
 				int i;
-				for (i = 0; i < hotel.length-1; i++){
+				for (i = 0; i < hotel.length - 1; i++) {
 					if (hotel[i] != null) {
 						System.out.println(hotel[i].name + "," + hotel[i].type);
-					} 
-				else {
+					} else {
 						System.out.println("空室");
 					}
 
@@ -113,38 +100,34 @@ public class Presentation {
 			else if (input_fn == 2) {
 				System.out.println("給食を始めます。部屋番号を入力してください。");
 				int input_room = sc_2.nextInt();
-				if (hotel[input_room - 1] != null){
+				if (hotel[input_room - 1] != null) {
 					if (hotel[input_room - 1].type == "DOG") {
 						myDog.setName(hotel[input_room - 1].name);
 						myDog.eat();
-					}
-					else {
+					} else {
 						myCat.setName(hotel[input_room - 1].name);
 						myCat.eat();
 					}
-					}
 				}
-
-			
+			}
 
 			// extra service
 			else if (input_fn == 3) {
 				System.out.println("エクストラサービスを始めます。部屋番号を入力してください。");
 				int input_room = sc_2.nextInt();
-				if(hotel[input_room - 1] != null) {
+				if (hotel[input_room - 1] != null) {
 					System.out.println("何サービスがほしいですか。犬、猫が風呂、トリミングが１、犬の散歩は２。");
 					int input_service = sc_1.nextInt();
 					if (input_service == 1) {
-						if(hotel[input_service - 1].type =="DOG") {
+						if (hotel[input_service - 1].type == "DOG") {
 							myDog.setName(hotel[input_room - 1].name);
 							myDog.trim_bath();
-						}
-						else {
+						} else {
 							myCat.setName(hotel[input_room - 1].name);
 							myCat.trim_bath();
 						}
 					}
-					if (input_service ==2) {
+					if (input_service == 2) {
 						myDog.walk();
 					}
 					;
@@ -155,42 +138,36 @@ public class Presentation {
 			else if (input_fn == 4) {
 				System.out.println("部屋番号を入力してください。");
 				int input_room = sc_2.nextInt();
-				hotel[Integer.valueOf(input_room) -1] = null;
+				hotel[Integer.valueOf(input_room) - 1] = null;
 				System.out.println("チェックアウト完了");
 
 			}
-			//意見
+			// 意見
 			else if (input_fn == 5) {
 				System.out.println("意見を入力してください。");
 				try {
-					 Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("D:\\Dolly\\Documents\\Workspace\\IotaJava_G4\\src\\co\\jp\\Part2_Presentation\\claim.txt"),
-					 "UTF-8"));
-					 String  input_claim= sc_3.nextLine();
-					 out.write(input_claim + c.getTime());
-					 out.close();
-					} catch (IOException e) {
-					 e.printStackTrace();
-					}
-			}
-			else if (input_fn == 6) {
+					Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(
+							"D:\\Dolly\\Documents\\Workspace\\IotaJava_G4\\src\\co\\jp\\Part2_Presentation\\claim.txt"),
+							"UTF-8"));
+					String input_claim = sc_3.nextLine();
+					out.write(input_claim + c.getTime());
+					out.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} else if (input_fn == 6) {
 				System.out.println("----- 名前を書いてください。 -----");
 				String input_vipname = sc_3.nextLine();
 				vip.add(input_vipname);
 				System.out.println("会員リスト" + vip);
 			}
-			
+
 			// Manually exit
 			else if (input_fn == 7) {
 				System.out.println("----- システム終了 -----");
 				break;
 			}
 
-			
-
-		
-			
 		}
 	}
 }
-
-
