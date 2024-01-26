408e52b1cf33bf867c4c55e6b1a146b37cbdd624
hubao <hubao@DESKTOP-5E2289V>
2024-01-26 18:51:23 +0900

    更新 hu 20240126 GITコミット練習

--- src/co/jp/chapter01/JavaChapter01.java
+++ src/co/jp/chapter01/JavaChapter01.java
@@ -5,7 +5,7 @@ public class JavaChapter01 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-		System.out.println("hello world");
+		System.out.println("hello world 678686876870980980980986776709809908968");
 
 	}
 }
